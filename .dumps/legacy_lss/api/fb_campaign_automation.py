269903db03a2d4507f45f0e1dc119a20fc3625a7
Ian Liu <ipuppyian@gmail.com>
2021-11-09 16:29:15 +0800

    house cleaning

--- .dumps/legacy_lss/api/fb_campaign_automation.py
+++ .dumps/legacy_lss/api/fb_campaign_automation.py
@@ -13,51 +13,6 @@ from api.utilities.fb_campaign_order_capturing import capture_fb_campaign_commen
 from config import connex_app
 
 
-@ connex_app.route('/fb_campaign_automation', methods=['POST'])
-# @jwt_required
-def fb_campaign_automation():
-    # Check the argument
-    controlpanelargs = request.args.get('controlpanelargs', None)
-    if not controlpanelargs:
-        return jsonify({"msg": "denied"}), 400
-    elif controlpanelargs == 'order_capturing':
-        fb_campaigns_by_now = orm_get_fb_campaigns_by_now(datetime.utcnow())
-        update_commnet_results = _get_update_commnet_results(
-            fb_campaigns_by_now)
-
-        return jsonify(
-            {"fb_campaigns_updated": len(fb_campaigns_by_now),
-             "update_commnet_results": update_commnet_results}
-        ), 200
-    elif controlpanelargs == 'stat':
-        fb_campaigns_by_now = orm_get_fb_campaigns_by_now(datetime.utcnow())
-        update_stat_results = _get_update_stat_results(
-            fb_campaigns_by_now)
-
-        return jsonify(
-            {"fb_campaigns_updated": len(fb_campaigns_by_now),
-             "update_stat_results": update_stat_results}
-        ), 200
-    elif controlpanelargs == 'campaign_summary':
-        fb_campaigns_by_end_time_delta = orm_get_fb_campaigns_by_end_time_delta(
-            datetime.utcnow(), timedelta(days=6))
-        fb_campaign_summaries = _get_fb_campaign_summaries(
-            fb_campaigns_by_end_time_delta)
-
-        return jsonify(
-            {"fb_campaign_summaries_updated": len(fb_campaigns_by_end_time_delta),
-             "fb_campaign_summaries": fb_campaign_summaries}
-        ), 200
-    elif controlpanelargs == 'generate_orders':
-        fb_campaigns_by_now = orm_get_fb_campaigns_by_now(datetime.utcnow())
-        generated_orders = _get_generated_orders(fb_campaigns_by_now)
-
-        return jsonify(
-            {"fb_campaigns_updated": len(fb_campaigns_by_now),
-             "generated_orders": generated_orders}
-        ), 200
-
-
 def _get_update_commnet_results(fb_campaigns_by_now):
     # TODO: threading here?
     update_commnet_results = []
