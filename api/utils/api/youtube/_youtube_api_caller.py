c484c81a0f0d014ad6f7fa6066463fa3daeb9f77
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-15 16:21:06 +0800

    youtube_module

--- api/utils/api/youtube/_youtube_api_caller.py
+++ api/utils/api/youtube/_youtube_api_caller.py
@@ -6,38 +6,3 @@ from api.utils.api._api_caller import RestApiJsonCaller
 @dataclass
 class YoutubeApiCaller(RestApiJsonCaller):
     domain_url: str = settings.YOUTUBE_API_URL
-
-
-def api_youtube_get_live_chat_comment(page_token: str, live_chat_id: str, max_results: int):
-    params = {
-        "key": settings.YOUTUBE_API_KEY,
-        "masResults": max_results,
-        "part": "id,snippet,authorDetails",
-        "liveChatId": live_chat_id,
-        "pageToken": page_token
-    }
-    ret = YoutubeApiCaller('liveChat/messages', params=params).get()
-    return ret
-
-
-def api_youtube_get_video_info(video_id: str):
-    params = {
-        "key": settings.YOUTUBE_API_KEY,
-        "id": video_id,
-        "part": "id,snippet,liveStreamingDetails",
-    }
-    ret = YoutubeApiCaller('videos', params=params).get()
-    return ret
-
-
-def api_youtube_get_video_comment(page_token: str, video_id: str, max_results: int):
-    params = {
-        "key": settings.YOUTUBE_API_KEY,
-        "id": video_id,
-        "masResults": max_results,
-        "part": "id,snippet",
-        "pageToken": page_token,
-        "order": "time"
-    }
-    ret = YoutubeApiCaller('commentThreads', params=params).get()
-    return ret
---
+++ api/utils/api/youtube/live_chat.py
@@ -0,0 +1,36 @@
+from api.utils.api.youtube._youtube_api_caller import YoutubeApiCaller
+from django.conf import settings
+
+
+def api_youtube_get_live_chat_comment(page_token: str, live_chat_id: str, max_results: int):
+    params = {
+        "key": settings.YOUTUBE_API_KEY,
+        "masResults": max_results,
+        "part": "id,snippet,authorDetails",
+        "liveChatId": live_chat_id,
+        "pageToken": page_token
+    }
+    ret = YoutubeApiCaller('liveChat/messages', params=params).get()
+    return ret
+
+
+def api_youtube_post_live_chat_comment(access_token: str, live_chat_id: str, text: str):
+
+    data = {
+        "snippet": {
+            "liveChatId": live_chat_id,
+            "type": "textMessageEvent",
+            "textMessageDetails": {
+                "messageText": text
+            }
+        }
+    }
+
+    params = {
+        "key": settings.YOUTUBE_API_KEY,
+        "part": "id,snippet",
+    }
+
+    ret = YoutubeApiCaller(
+        'liveChat/messages', bearer_token=access_token, params=params, data=data).post()
+    return ret
