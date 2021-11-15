37e1286e5fdcf20b101c25447be747386074ad88
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-15 15:29:19 +0800

    youtube_module

--- lss/settings.py
+++ lss/settings.py
@@ -231,7 +231,8 @@ FACEBOOK_APP_CREDS = {
 }
 
 # Youtube
-YOUTUBE_API_URL = ""
+YOUTUBE_API_KEY = "AIzaSyDcsngr4WWNI5HUi9CNMLVRgCx5YdTaaA0"
+YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3"
 YOUTUBE_API_CONFIG = {
     "web": {
         "project_id": "primeval-nectar-322805",
