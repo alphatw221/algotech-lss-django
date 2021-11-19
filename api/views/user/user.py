706bce176f033e27445de058a02baaa6bebe4fca
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-19 19:00:59 +0800

    bind_facebook_page

--- api/views/user/user.py
+++ api/views/user/user.py
@@ -6,6 +6,8 @@ from api.models.user.user import User, UserSerializer
 from api.views.user._user import login_helper
 
 from api.models.facebook.facebook_page import FacebookPage
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -23,28 +25,26 @@ class UserViewSet(viewsets.ModelViewSet):
     def user_login(self, request, pk=None):
         return login_helper(self, request, 'user', pk=None)
 
-    # @action(detail=False, methods=['POST'], url_path=r'bind_facebook_page')
-    # def bind_facebook_page(self, request, pk=None):
+    @action(detail=False, methods=['POST'], url_path=r'bind_facebook_page')
+    def bind_facebook_page(self, request, pk=None):
 
-    #     api_user = request.user.api_users.get(type='user')
+        items = request.data.get('data')
+        api_user = request.user.api_users.get(type='user')
+        api_user.facebook_info['pages'] = {}
+        for item in items:
+            page_token = item['access_token']
+            page_id = item['id']
+            page_name = item['name']
 
-    #     facebook_info = api_user.facebook_info
-    #     items = request.data.get('data')
+            if FacebookPage.objects.filter(page_id=page_id).exists():
+                FacebookPage.objects.filter(page_id=page_id).update(token=page_token, token_update_at=datetime.now(
+                ), token_update_by=api_user.facebook_info['id'])
+            else:
+                FacebookPage.objects.create(
+                    page_id=page_id, name=page_name, token=page_token, token_update_at=datetime.now(), token_update_by=api_user.facebook_info['id'])
 
-    #     pages = {}
-    #     for item in items:
-    #         page_token = item['access_token']
-    #         page_id = item['id']
-    #         page_name = item['name']
+            api_user.facebook_info['pages'][page_id] = item
 
-    #         if FacebookPage.objects.filter(page_id=page_id).exists():
-    #             facebook_page = FacebookPage.objects.create(
-    #                 page_id=page_id, name=page_name, token=page_token, token_update_at=datetime.now(), token_update_by=str(api_user.id))
-    #         else:
-    #             facebook_page = FacebookPage.objects.get(page_id=page_id)
-    #             facebook_page.update(token=page_token, token_update_at=datetime.now(
-    #             ), token_update_by=str(api_user.id))
+        api_user.save()
 
-    #         pages[page_id]=item
-
-    #     return Response(ret, status=status.HTTP_200_OK)
+        return Response('complete', status=status.HTTP_200_OK)
