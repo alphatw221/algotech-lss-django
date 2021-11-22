68942ef1659546e63213328bdbda41c03e7e162b
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-22 14:26:38 +0800

    product_add_paginnation

--- api/views/campaign/campaign_product.py
+++ api/views/campaign/campaign_product.py
@@ -1,6 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from api.models.campaign.campaign_product import CampaignProduct, CampaignProductSerializer
+
+
+class CampaignProductPagination(LimitOffsetPagination):
+
+    default_limit = 25
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
+    max_limit = 100
 
 
 class CampaignProductViewSet(viewsets.ModelViewSet):
@@ -8,3 +17,4 @@ class CampaignProductViewSet(viewsets.ModelViewSet):
     queryset = CampaignProduct.objects.all().order_by('id')
     serializer_class = CampaignProductSerializer
     filterset_fields = []
+    pagination_class = CampaignProductPagination
--- api/views/product/product.py
+++ api/views/product/product.py
@@ -1,6 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from api.models.product.product import Product, ProductSerializer
+
+
+class ProductPagination(LimitOffsetPagination):
+
+    default_limit = 25
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
+    max_limit = 100
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -8,3 +17,4 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
     filterset_fields = []
+    pagination_class = ProductPagination
