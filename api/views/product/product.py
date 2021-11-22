ebe9b9d8ee78fadb8b99855d495c1c2879576adc
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-22 19:28:54 +0800

    product_pagination

--- api/views/product/product.py
+++ api/views/product/product.py
@@ -2,7 +2,7 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.response import Response
-from api.models import product
+from api.models import order, product
 from api.models.product.product import Product, ProductSerializer
 from rest_framework.decorators import action
 
@@ -42,7 +42,16 @@ class ProductViewSet(viewsets.ModelViewSet):
             return Response({"message": "no user found"}, status=status.HTTP_400_BAD_REQUEST)
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
-        response = self.queryset.filter(status=product_status,
-                                        name__icontains=key_word).order_by(order_by)
 
-        return Response(response, status=status.HTTP_200_OK)
+        queryset = self.queryset
+        try:
+            if product_status:
+                queryset = queryset.filter(status=product_status)
+            if key_word:
+                queryset = queryset.filter(name__icontains=key_word)
+            if order_by:
+                queryset = queryset.order_by(order_by)
+        except:
+            return Response({"message": "query error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response(queryset.all(), status=status.HTTP_200_OK)
