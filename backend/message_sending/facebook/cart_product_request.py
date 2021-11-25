f741c0ecd11c80a7189e4bbd873c59e1d4227fc4
Ian Liu <ipuppyian@gmail.com>
2021-11-25 13:26:41 +0800

    CampaignAnnouncementFacebookMessageAgent

--- backend/message_sending/facebook/cart_product_request.py
+++ backend/message_sending/facebook/cart_product_request.py
@@ -1,5 +1,4 @@
 from backend.api.facebook.post import (api_fb_post_page_comment_on_comment,
-                                       api_fb_post_page_comment_on_post,
                                        api_fb_post_page_message_on_comment)
 
 
@@ -13,8 +12,3 @@ class CartProductRequestFacebookMessageAgent:
     def page_message_on_comment(page_token: str, comment_id: str, message_text: str):
         return api_fb_post_page_message_on_comment(
             page_token, comment_id, message_text)
-
-    @staticmethod
-    def page_comment_on_post(page_token: str, post_id: str, message_text: str):
-        return api_fb_post_page_comment_on_post(
-            page_token, post_id, message_text)
