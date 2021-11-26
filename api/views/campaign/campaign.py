9c87c59c1ce6ff8f446274cc88fe2da19e84f58d
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-11-26 10:22:18 +0800

    lin

--- api/views/campaign/campaign.py
+++ api/views/campaign/campaign.py
@@ -2,11 +2,15 @@ from functools import partial
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from api.models.campaign.campaign import Campaign, CampaignSerializer
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
-import io
-from rest_framework.parsers import JSONParser
+
+from api.models.facebook.facebook_page import FacebookPage
+from api.models.youtube.youtube_channel import YoutubeChannel
+from backend.api.facebook.user import api_fb_get_me_accounts
+platform_dict = {'facebook': FacebookPage,
+                 'youtube': YoutubeChannel}
 
 
 class CampaignPagination(PageNumberPagination):
@@ -25,6 +29,9 @@ class CampaignViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['GET'], url_path=r'retrieve_campaign')
     def retrieve_campaign(self, request, pk=None):
 
+        platform_name = request.query_params.get('platform_name')
+        platform_id = request.query_params.get('platform_id')
+
         api_user = request.user.api_users.get(type='user')
         # TODO 檢查
         if not api_user:
@@ -32,7 +39,18 @@ class CampaignViewSet(viewsets.ModelViewSet):
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not api_user.campaigns.filter(id=pk).exists():
+        if platform_name not in platform_dict:
+            return Response({"message": "no platfrom name found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform_dict[platform_name].objects.filter(id=platform_id).exists():
+            return Response({"message": "no platfrom found"}, status=status.HTTP_400_BAD_REQUEST)
+        platform = platform_dict[platform_name].objects.get(
+            id=platform_id)
+
+        if not is_admin(platform_name, api_user, platform):
+            return Response({"message": "user is not platform admin"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform.campaigns.filter(id=pk).exists():
             return Response({"message": "no campaign found"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
@@ -46,6 +64,9 @@ class CampaignViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['GET'], url_path=r'list_campaign')
     def list_campaign(self, request):
 
+        platform_name = request.query_params.get('platform_name')
+        platform_id = request.query_params.get('platform_id')
+
         order_by = request.query_params.get('order_by')
         product_status = request.query_params.get('status')
         key_word = request.query_params.get('key_word')
@@ -57,7 +78,18 @@ class CampaignViewSet(viewsets.ModelViewSet):
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
 
-        campaigns = api_user.campaigns.all()
+        if platform_name not in platform_dict:
+            return Response({"message": "no platfrom name found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform_dict[platform_name].objects.filter(id=platform_id).exists():
+            return Response({"message": "no platfrom found"}, status=status.HTTP_400_BAD_REQUEST)
+        platform = platform_dict[platform_name].objects.get(
+            id=platform_id)
+
+        if not is_admin(platform_name, api_user, platform):
+            return Response({"message": "user is not platform admin"}, status=status.HTTP_400_BAD_REQUEST)
+
+        campaigns = platform.campaigns.all()
         try:
             if product_status:
                 campaigns = campaigns.filter(status=product_status)
@@ -82,6 +114,9 @@ class CampaignViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['POST'], url_path=r'create_campaign')
     def create_campaign(self, request):
 
+        platform_name = request.query_params.get('platform_name')
+        platform_id = request.query_params.get('platform_id')
+
         api_user = request.user.api_users.get(type='user')
         # TODO 檢查
         if not api_user:
@@ -89,9 +124,23 @@ class CampaignViewSet(viewsets.ModelViewSet):
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
 
+        if platform_name not in platform_dict:
+            return Response({"message": "no platfrom name found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform_dict[platform_name].objects.filter(id=platform_id).exists():
+            return Response({"message": "no platfrom found"}, status=status.HTTP_400_BAD_REQUEST)
+        platform = platform_dict[platform_name].objects.get(
+            id=platform_id)
+
+        if not is_admin(platform_name, api_user, platform):
+            return Response({"message": "user is not platform admin"}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
             data = request.data
             data['created_by'] = api_user.id
+            # TODO 之後要改寫
+            data['facebook_page'] = platform.id if platform_name == 'facebook' else None
+            data['youtube_channel'] = platform.id if platform_name == 'youtube' else None
             serializer = self.get_serializer(data=data)
 
             if not serializer.is_valid():
@@ -106,6 +155,9 @@ class CampaignViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['PUT'], url_path=r'update_campaign')
     def update_campaign(self, request, pk=None):
 
+        platform_name = request.query_params.get('platform_name')
+        platform_id = request.query_params.get('platform_id')
+
         api_user = request.user.api_users.get(type='user')
         # TODO 檢查
         if not api_user:
@@ -113,9 +165,20 @@ class CampaignViewSet(viewsets.ModelViewSet):
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not api_user.campaigns.filter(id=pk).exists():
+        if platform_name not in platform_dict:
+            return Response({"message": "no platfrom name found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform_dict[platform_name].objects.filter(id=platform_id).exists():
+            return Response({"message": "no platfrom found"}, status=status.HTTP_400_BAD_REQUEST)
+        platform = platform_dict[platform_name].objects.get(
+            id=platform_id)
+
+        if not is_admin(platform_name, api_user, platform):
+            return Response({"message": "user is not platform admin"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform.campaigns.filter(id=pk).exists():
             return Response({"message": "no campaign found"}, status=status.HTTP_400_BAD_REQUEST)
-        campaign = api_user.campaigns.get(id=pk)
+        campaign = platform.campaigns.get(id=pk)
 
         try:
             serializer = self.get_serializer(
@@ -132,6 +195,9 @@ class CampaignViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['DELETE'], url_path=r'delete_campaign')
     def delete_campaign(self, request, pk=None):
 
+        platform_name = request.query_params.get('platform_name')
+        platform_id = request.query_params.get('platform_id')
+
         api_user = request.user.api_users.get(type='user')
         # TODO 檢查
         if not api_user:
@@ -139,7 +205,18 @@ class CampaignViewSet(viewsets.ModelViewSet):
         elif api_user.status != "valid":
             return Response({"message": "not activated user"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not api_user.campaigns.filter(id=pk).exists():
+        if platform_name not in platform_dict:
+            return Response({"message": "no platfrom name found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform_dict[platform_name].objects.filter(id=platform_id).exists():
+            return Response({"message": "no platfrom found"}, status=status.HTTP_400_BAD_REQUEST)
+        platform = platform_dict[platform_name].objects.get(
+            id=platform_id)
+
+        if not is_admin(platform_name, api_user, platform):
+            return Response({"message": "user is not platform admin"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not platform.campaigns.filter(id=pk).exists():
             return Response({"message": "no campaign found"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
@@ -148,3 +225,20 @@ class CampaignViewSet(viewsets.ModelViewSet):
             return Response({"message": "error occerd during deleting"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({"message": "delete success"}, status=status.HTTP_200_OK)
+
+
+def is_admin(platform_name, api_user, platform):
+    try:
+        if platform_name == 'facebook':
+            status_code, response = api_fb_get_me_accounts(
+                api_user.facebook_info['token'])
+
+            for item in response['data']:
+                if item['id'] == platform.page_id:
+                    return True
+            return False
+        elif platform_name == 'youtube':
+            pass
+    except:
+        return False
+    return False
