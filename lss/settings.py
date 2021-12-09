ce243c94d786cc05c4c1a4b5ef62fd0b44e22f07
Ian Liu <ipuppyian@gmail.com>
2021-12-09 16:12:53 +0800

    update settings.py

--- lss/settings.py
+++ lss/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+
 from google.oauth2 import service_account
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
