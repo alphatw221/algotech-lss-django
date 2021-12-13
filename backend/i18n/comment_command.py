dabc391cce64e755eb7ce4c5c5861749c34ba7eb
Yi-Hsueh Lin <yihsuehlin@accoladeglobal.net>
2021-12-13 17:49:31 +0800

    test i18n

--- backend/i18n/comment_command.py
+++ backend/i18n/comment_command.py
@@ -39,20 +39,21 @@ def _i18n_get_cart_products(cart_products: list[CartProduct]):
     items = []
     subtotal = 0
     count = len(cart_products)
-    digits = len(str(count))
+    digits = 2 if count < 100 else len(str(count))
     for i, cart_product in enumerate(cart_products):
         name = getattr(cart_product.campaign_product, 'name', '')
         qty = getattr(cart_product, 'qty', 0)
         total = Decimal(
             getattr(cart_product.campaign_product, 'price', '0')) * qty
-        items.append(f"{str(i).zfill(digits)}. " +
-                     _('ITEM_INFO{name}{qty}{dollar_sign}{total}\n').format(
-                         name=name,
-                         qty=qty,
-                         dollar_sign=cart_product.campaign_product.currency_sign,
-                         total=total
-                     )
-                     )
+        items.append(
+            f"{str(i).zfill(digits)}. " +
+            _('ITEM_INFO{name}{qty}{dollar_sign}{total}\n').format(
+                name=name,
+                qty=qty,
+                dollar_sign=cart_product.campaign_product.currency_sign,
+                total=total
+            )
+        )
         subtotal += total
     return items, subtotal
 
