780f806799c6bd6f5e18e7d4a808b668fdd1048e
asdf50003206 <asdf500032061@gmail.com>
2021-12-27 21:50:49 +0800

    edit settings

--- lss/settings.py
+++ lss/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-_*%^a_086!sv_#y^t(c0(+%dbqufars4zf8##q!yqjlp#c7m!l
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0',
-                 "gipassl.algotech.app", ]
+                 "gipassl.algotech.app", '104.199.211.63']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
