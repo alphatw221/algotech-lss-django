53bf51573863726334b658116f60bbce98150165
asdf50003206 <asdf500032061@gmail.com>
2021-12-27 17:27:17 +0800

    wsgi.py sys path append

--- lss/wsgi.py
+++ lss/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+#------add thoes line if server with apache--------
+import sys
+sys.path.append('/home/lss_env/lib/python3.9/site-packages')
+#--------------------------------------------------
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lss.settings')
 
 application = get_wsgi_application()
