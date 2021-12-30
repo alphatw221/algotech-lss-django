8992ed0989100fbf305b848272b9912202b38e0a
asdf50003206 <asdf500032061@gmail.com>
2021-12-30 20:47:25 +0800

    auto incomplete

--- backend/pymongo/mongodb.py
+++ backend/pymongo/mongodb.py
@@ -14,7 +14,7 @@ def get_incremented_filed(collection_name, field_name):
             with session.start_transaction():
                 doc=db.auto_increment.find_one({"collection_name":collection_name, "field_name":field_name}, session=session)
                 increased_number=doc['number']+1
-                db.auto_increment.update_one({"collection_name":collection_name, "field_name":field_name},{"$set":{"number":incremented_number}}, session=session)
+                db.auto_increment.update_one({"collection_name":collection_name, "field_name":field_name},{"$set":{"number":increased_number}}, session=session)
     except Exception as e:
         raise DBException
     return increased_number
