6183105ba4f856ce178f21b17232b33200db197b
asdf50003206 <asdf500032061@gmail.com>
2022-01-26 11:11:47 +0800

    update youtube get comment

--- api/views/youtube/youtube_channel.py
+++ api/views/youtube/youtube_channel.py
@@ -41,7 +41,7 @@ class YoutubeViewSet(viewsets.GenericViewSet):
 
         # comments = db.api_campaign_comment.find({"campaign_id":int(campaign_id),"platform":"youtube"},{'_id': False}).limit(count)
 
-        comments = db.api_campaign_comment.find({"campaign_id":int(campaign_id),"platform":"facebook"},{'_id': False}).limit(count)
+        comments = db.api_campaign_comment.find({"campaign_id":int(campaign_id),"platform":"youtube"},{'_id': False}).limit(count)
 
         comments_str = dumps(comments)
         comments_json = loads(comments_str)
