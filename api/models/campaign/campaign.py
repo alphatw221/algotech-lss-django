1eecdf6d223515596e727890a31a02d494e8eeb2
asdf50003206 <asdf500032061@gmail.com>
2022-01-26 11:01:46 +0800

    update youtube campaign serializer

--- api/models/campaign/campaign.py
+++ api/models/campaign/campaign.py
@@ -66,7 +66,7 @@ class FacebookCampaignSerializer(serializers.Serializer):
 class YoutubeCampaignSerializer(serializers.Serializer):
     live_video_id = serializers.CharField(required=False, default="")
     is_failed = serializers.BooleanField(required=False, default=False)
-    latest_comment_time = serializers.IntegerField(required=False, default=1)
+    latest_comment_time = serializers.CharField(required=False, default="")
     remark = serializers.CharField(required=False, default="")
 
 
