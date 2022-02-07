ea1a9dd057fe2e15bb3dbc0fea492a9904bbf0fd
root <root@LAPTOP-5LMCTULN.localdomain>
2022-02-07 16:14:56 +0800

    setting mail hist changed

--- lss/settings.py
+++ lss/settings.py
@@ -305,7 +305,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'derekhwang@accoladeglobal.net'
+EMAIL_HOST_USER = 'noreply@tig.algotech.app'
 EMAIL_HOST_PASSWORD = 'jyhudyfbvpmewjsc'
 
 # cron app
