039a2ae40d7a83579fd8ddbd203c2b4311522713
Nick LIen <nicklien@accoladeglobal.net>
2022-02-10 19:20:03 +0800

    fix google login callback api

--- api/views/user/_user.py
+++ api/views/user/_user.py
@@ -91,7 +91,7 @@ def google_login_helper(request, user_type='customer'):
             "code": code,
             "client_id": "536277208137-okgj3vg6tskek5eg6r62jis5didrhfc3.apps.googleusercontent.com",
             "client_secret": "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",
-            "redirect_uri": "http://localhost:8001/api/user/google_user_callback",
+            "redirect_uri": settings.GCP_API_LOADBALANCER_URL + "/api/user/google_user_callback",
             "grant_type": "authorization_code"
         }
     )
