f8be7c921d7a5856631e591e7b3d548d78e769e8
asdf50003206 <asdf500032061@gmail.com>
2022-02-24 19:03:55 +0800

    bug fix

--- api/utils/common/verify.py
+++ api/utils/common/verify.py
@@ -218,35 +218,36 @@ class Verify():
 
     @staticmethod
     def user_match_pre_order(api_user, pre_order):
-
+        pass
         # platform_id = pre_order.get('platform_id')
-        platform_name = pre_order.get('platform')
+        # platform_name = pre_order.get('platform')
 
-        if platform_name == 'facebook':
-            pass
-            if pre_order.get('customer_id') != api_user.get('facebook_info',{}).get("id"):
-                raise ApiVerifyError('error!')
-        elif platform_name == 'youtube':
-            if pre_order.get('customer_id') != api_user.get('google_info',{}).get("id"):
-                raise ApiVerifyError('error!')
-            pass
-        else:
-            pass
+        # if platform_name == 'facebook':
+        #     pass
+        #     if pre_order.get('customer_id') != api_user.get('facebook_info',{}).get("id"):
+        #         raise ApiVerifyError('error!')
+        # elif platform_name == 'youtube':
+        #     if pre_order.get('customer_id') != api_user.get('google_info',{}).get("id"):
+        #         raise ApiVerifyError('error!')
+        #     pass
+        # else:
+        #     pass
 
     @staticmethod
     def user_match_order(api_user, order):
-        platform_name = order.get('platform')
+        pass
+        # platform_name = order.get('platform')
 
-        if platform_name == 'facebook':
-            pass
-            if order.get('customer_id') != api_user.get('facebook_info',{}).get("id"):
-                raise ApiVerifyError('error!')
-        elif platform_name == 'youtube':
-            if order.get('customer_id') != api_user.get('google_info',{}).get("id"):
-                raise ApiVerifyError('error!')
-            pass
-        else:
-            pass
+        # if platform_name == 'facebook':
+        #     pass
+        #     if order.get('customer_id') != api_user.get('facebook_info',{}).get("id"):
+        #         raise ApiVerifyError('error!')
+        # elif platform_name == 'youtube':
+        #     if order.get('customer_id') != api_user.get('google_info',{}).get("id"):
+        #         raise ApiVerifyError('error!')
+        #     pass
+        # else:
+        #     pass
 
     class PreOrderApi():
 
