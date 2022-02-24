9d1222d4abd6a4e9245ceac53faeb0152a4a83c7
asdf50003206 <asdf500032061@gmail.com>
2022-02-24 11:02:26 +0800

    update permission

---
+++ api/permissions/seller/order_permission.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+from api.utils.common.verify import Verify
+from api.utils.common.common import getparams
+
+
+class IsOrderSeller(BasePermission):
+
+    def has_permission(self, request, view):
+        try:
+            pk = view.kwargs.get('pk')
+            api_user, platform_id, platform_name = getparams(request, ('platform_id', 'platform_name'), with_user=True, seller=True)
+
+            platform = Verify.get_platform(api_user, platform_name, platform_id)
+            order = Verify.get_order(pk)
+            Verify.get_campaign_from_platform(platform, order.campaign.id)
+            
+        except Exception:
+            return False
+        return True
+
+
