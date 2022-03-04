ce5ab2b65d94a033a3eeac9483f57178d2bb797c
root <root@LAPTOP-5LMCTULN.localdomain>
2022-03-04 10:38:18 +0800

    refactor: convert 1.0 to 1.1 db module & test template

--- automation/management/commands/t.py
+++ automation/management/commands/t.py
@@ -119,6 +119,16 @@ class Command(BaseCommand):
         
         sql6 = 'select * from lss_fb_page_meta where fb_page_id="528158520664119"'
 
+        # lss_campaign_command = 'select * from lss_fb_campaign where fb_page_id="528158520664119"'
+        # cursor.execute(lss_campaign_command)
+        # arimee_campaigns = cursor.fetchall()
+
+        # for campaign in arimee_campaigns:
+        #     db.api_campaign.update({'title': campaign['title']}, 
+        #     {'$set': {'start_at': campaign['start_time'], 'end_at': campaign['end_time']}}) 
+
+
+
         ## -------- insert product
         # sql7 = 'SELECT * FROM lss_product WHERE fb_user_id IN ("468936317671599", "10159642927732044")'
         # cursor7.execute(sql7)
@@ -151,11 +161,10 @@ class Command(BaseCommand):
         #     )
         #     prod.save()
 
-
         
 
         ## ----------- insert api_campaign
-        lss_campaign_command = 'select * from lss_fb_campaign where fb_page_id="528158520664119"'
+        lss_campaign_command = 'select * from lss_fb_campaign where fb_page_id="1679082475665513"'
         cursor.execute(lss_campaign_command)
         arimee_campaigns = cursor.fetchall()
 
@@ -165,8 +174,10 @@ class Command(BaseCommand):
             campaign = Campaign.objects.create(
                 title=campaign['title'], 
                 description=campaign['description'], 
+                start_at=campaign['start_time'],
+                end_at=campaign['end_time'],
                 created_at=campaign['date_added'], 
-                updated_at=campaign['date_added'], 
+                updated_at=campaign['date_modified'], 
                 facebook_campaign={"post_id":campaign['fb_post_id']}, 
                 facebook_page_id=25, 
                 created_by_id=66, 
@@ -229,7 +240,6 @@ class Command(BaseCommand):
                     if sql_order_meta['meta_key'] == 'free_delivery_by_campaign_admin':
                         free_delivery_by_campaign_admin = True if sql_order_meta.get('meta_value')=="true" else False
 
-
                 shipping_first_name, shipping_last_name, shipping_phone, shipping_email = '', '', '', ''
                 shipping_post_code, shipping_address, delivery_way, shipping_remarks, shipping_date = '', '', '', '', None
                 sql3 = 'select * from lss_order_user_meta where order_id=' + str(order_id)
@@ -255,9 +265,12 @@ class Command(BaseCommand):
                         shipping_remarks = user_meta['meta_value']
                     if user_meta['meta_key'] == 'shipping_date':
                         shipping_date = user_meta['meta_value'][:10] if user_meta['meta_value'] else None
+                        try:
+                            shipping_date = datetime.datetime.strptime(shipping_date, '%y/%m/%d %H:%M:%S')
+                        except:
+                            shipping_date = None
                     if user_meta['meta_key'] == 'delivery_way':
                         delivery_way = user_meta['meta_value']
-                
             
                 order = Order.objects.create(
                     campaign=campaign, customer_id=customer_id, customer_name=customer_name, customer_img=image,
@@ -273,7 +286,6 @@ class Command(BaseCommand):
                 sql4 = 'select * from lss_order_product where order_id=' + str(order_id)
                 cursor4.execute(sql4)
                 user_products = cursor4.fetchall()
-
                 
                 products={}
                 for user_product in user_products:
@@ -284,14 +296,15 @@ class Command(BaseCommand):
                             subtotal=user_product['total']
                         )
 
-                        products[str(order_product.id)]={
-                            "campaign_product_id":order_product.campaign_product.id,
+                        products[str(order_product.campaign_product.id)]={
+                            "order_product_id": order_product.id,
+                            # "campaign_product_id":order_product.campaign_product.id,
                             "name":order_product.name,
                             # "image":order_product.image,
                             "price":order_product.price,
                             "type":order_product.type,
                             "currency" : None,
-                            # "currency_sign" : order_product,
+                            "currency_sign" : "$",
                             "qty" : order_product.qty,
                             "subtotal" : order_product.subtotal
                         }
@@ -301,7 +314,28 @@ class Command(BaseCommand):
                 order.products=products
                 order.save()
 
-        
+
+
+        # arimee_orders = db.api_order.find({'campaign_id': {'$in': [228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251]}})
+        # for order in arimee_orders:
+        #     products = order['products']
+        #     order_id = order['id']
+
+        #     new_products = {}
+        #     for id, product in products.items():
+        #         # campaign_product_id = product['campaign_product_id']
+        #         # product['order_product_id'] = int(order_product_id)
+        #         # product.pop('campaign_product_id')
+        #         product['currency_sign'] = '$'
+        #         # print (id, product)
+        #         new_products[id] = product
+            
+        #     print (order_id)
+        #     db.api_order.update({'id': order_id}, {'$set': {'products': new_products}})
+
+
+
+
 
     def campaign_test(self):
         cs = CampaignManager.get_active_campaigns()
