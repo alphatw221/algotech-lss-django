543a0351b2da61f967a85afc676548e309f239fa
Nick LIen <nicklien@accoladeglobal.net>
2022-03-22 13:16:19 +0800

    separate google_login_helper

--- api/views/user/_user.py
+++ api/views/user/_user.py
@@ -207,22 +207,28 @@ def verify_google_user(response, user_type):
     return ret
 
 def google_login_helper(request, user_type='customer'):
-    
+    token = request.query_params.get("token", None)
+    identity_info = id_token.verify_oauth2_token(token, google_requests.Request(), settings.GOOGLE_OAUTH_CLIENT_ID_FOR_LIVESHOWSELLER)
+    our_jwt_tokens = verify_google_user(identity_info, user_type=user_type)
+    return Response(our_jwt_tokens, status=status.HTTP_200_OK)
+
+
+# for v2 use
+def google_login_helper_v2(request, user_type='customer'):
     if user_type=='customer':
         token = request.query_params.get("token", None)
-        identity_info = id_token.verify_oauth2_token(token, google_requests.Request(), settings.GOOGLE_OAUTH_CLIENT_ID_FOR_LIVESHOWSELLER)
-    
+        identity_info = id_token.verify_oauth2_token(token, google_requests.Request(),
+                                                        settings.GOOGLE_OAUTH_CLIENT_ID_FOR_LIVESHOWSELLER)
+
         our_jwt_tokens = verify_google_user(identity_info, user_type=user_type)
         return Response(our_jwt_tokens, status=status.HTTP_200_OK)
-
-
+    
     elif user_type=='user':
 
         state,google_user_code = getparams(request,("state","code"), with_user=False)
         # redirect_uri, callback_uri = state.split(',')
         state = json.loads(state)
         redirect_uri, redirect_route, callback_uri = state.get('redirect_uri'),state.get('redirect_route'),state.get('callback_uri')
-
 
         # google_user_code = request.data.get('code')
         print(redirect_uri)
@@ -239,7 +245,6 @@ def google_login_helper(request, user_type='customer'):
                     "grant_type": "authorization_code"
                 }
             )
-
 
         if not response.status_code / 100 == 2:
             print(response.json())
@@ -302,7 +307,7 @@ def google_login_helper(request, user_type='customer'):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         }
-        
+
         # return Response(ret, status=status.HTTP_200_OK)
 
         redirect = HttpResponseRedirect(redirect_to=redirect_uri+'#/'+redirect_route)
@@ -312,115 +317,6 @@ def google_login_helper(request, user_type='customer'):
         # # red.set_cookie('token', str(refresh.access_token)) #TODO setting domain, expired
         # red.set_cookie("access_token", str(refresh.access_token), path="/", domain=None, samesite=None, secure=False)
         # return red
-    
-
-
-# def google_login_helper(request, user_type='customer'):
-
-#     google_code = request.query_params.get("code")
-#     print("google_code", google_code)
-    
-#     response = requests.post(
-#             url="https://accounts.google.com/o/oauth2/token",
-#             data={
-#                 "code": request.query_params.get("code"),
-#                 "client_id": "536277208137-okgj3vg6tskek5eg6r62jis5didrhfc3.apps.googleusercontent.com",
-#                 "client_secret": "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",
-#                 # "redirect_uri": 'http://localhost:8000/google-redirect',
-#                 "redirect_uri": settings.WEB_SERVER_URL + "/google-redirect",
-#                 "grant_type": "authorization_code"
-#             }
-#         )
-
-
-#     if not response.status_code / 100 == 2:
-#         print(response.json())
-#         raise ApiCallerError('get google token fail')
-
-#     access_token = response.json().get("access_token")
-#     refresh_token = response.json().get("refresh_token")
-
-#     code, response = api_google_get_userinfo(access_token)
-
-#     if code / 100 != 2:
-#         print(response)
-#         print(traceback.format_exc)
-#         raise ApiCallerError("google user token invalid")
-
-#     google_id, google_name, google_picture, email = response["id"], response["name"], response["picture"], response["email"]
-#     # google_name = response["name"]
-#     # google_picture = response["picture"]
-#     # email = response["email"]
-
-#     api_user_exists = User.objects.filter(
-#         email=email, type=user_type).exists()
-#     auth_user_exists = AuthUser.objects.filter(email=email).exists()
-
-#     scenario1 = api_user_exists and auth_user_exists
-#     scenario2 = api_user_exists and not auth_user_exists
-#     scenario3 = not api_user_exists and auth_user_exists
-
-#     # scenario4: both don't exists
-
-#     if scenario1:
-#         api_user = User.objects.get(email=email, type=user_type)
-#         auth_user = api_user.auth_user
-#     elif scenario2:
-#         api_user = User.objects.get(email=email, type=user_type)
-#         auth_user = AuthUser.objects.create_user(
-#             google_name, email, ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(8)))
-#         api_user.auth_user = auth_user
-#     elif scenario3:
-#         auth_user = AuthUser.objects.get(email=email)
-#         api_user = User.objects.create(
-#             name=google_name, email=email, type=user_type, status='new', auth_user=auth_user)
-#     else:  # scenario4
-#         auth_user = AuthUser.objects.create_user(
-#             google_name, email, ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(8)))
-#         api_user = User.objects.create(
-#             name=google_name, email=email, type=user_type, status='new', auth_user=auth_user)
-
-#     if user_type == 'user' and api_user.status != 'valid':
-#         raise ApiVerifyError('account not activated')
-
-#     api_user.google_info["access_token"] = access_token
-#     api_user.google_info['refresh_token'] = refresh_token
-#     api_user.google_info["id"] = google_id
-#     api_user.google_info["name"] = google_name
-#     api_user.google_info["picture"] = google_picture
-
-    
-#     code, list_channel_response = api_youtube_get_list_channel_by_token(access_token)
-    
-#     if code / 100 == 2:
-#         channels = {}
-#         try:
-#             for item in list_channel_response.get("items"):
-#                 channels[item['id']] = item
-#             api_user.youtube_info['channels'] = channels
-#         except:
-#             print("this account doesn't have any channel.")
-#             api_user.youtube_info['channels'] = {
-#                 "message": "this account doesn't have any channel."
-#             }
-
-#     auth_user.last_login = datetime.now()
-#     auth_user.save()
-#     api_user.save()
-
-
-#     refresh = CustomTokenObtainPairSerializer.get_token(auth_user)
-
-#     ret = {
-#         'refresh': str(refresh),
-#         'access': str(refresh.access_token),
-#     }
-
-#     return Response(ret, status=status.HTTP_200_OK)
-
-    # # red.set_cookie('token', str(refresh.access_token)) #TODO setting domain, expired
-    # red.set_cookie("access_token", str(refresh.access_token), path="/", domain=None, samesite=None, secure=False)
-    # return red
 
 
 def instagram_login_helper(request, user_type='customer'):
