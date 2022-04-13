ef204da62d827282933ebac639043457c8e82327
asdf50003206 <asdf500032061@gmail.com>
2022-04-14 00:47:44 +0800

    update order helper

--- api/utils/common/order_helper.py
+++ api/utils/common/order_helper.py
@@ -358,7 +358,8 @@ class PreOrderHelper():
                     {'id': api_pre_order['id']},
                     {
                         "$set": {
-                            f"products.{str(api_campaign_product['id'])}": order_product,
+                            # f"products.{str(api_campaign_product['id'])}": order_product,
+                            "products.test": order_product,
                             # "subtotal":subtotal,
                             # "total":total
                         },
