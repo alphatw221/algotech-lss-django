07173cd6b38ddbddb181518dda4b656c83434495
root <root@LAPTOP-5LMCTULN.localdomain>
2022-04-14 11:00:05 +0800

    update chat box ph

--- chat_bot/views/facebook.py
+++ chat_bot/views/facebook.py
@@ -1,5 +1,7 @@
 from chat_bot.facebook.webhook import facebook_receive, facebook_verify
 from rest_framework.decorators import api_view
+import requests
+from django.conf import settings
 
 
 @api_view(['GET', 'POST'])
@@ -7,4 +9,7 @@ def webhook_facebook(request):
     if request.method == 'GET':
         return facebook_verify(request)
     elif request.method == 'POST':
+        if settings.GCP_API_LOADBALANCER_URL != "https://sb.liveshowseller.ph":
+            r = requests.post('https://sb.liveshowseller.ph/chat_box/facebook/', params=request.POST)
+
         return facebook_receive(request)
