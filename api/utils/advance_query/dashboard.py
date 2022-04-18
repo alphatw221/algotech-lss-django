d733a9540209c6dfa05c3546adc6befc99d81959
asdf50003206 <asdf500032061@gmail.com>
2022-04-18 14:39:27 +0800

    update dashboard

--- api/utils/advance_query/dashboard.py
+++ api/utils/advance_query/dashboard.py
@@ -276,3 +276,225 @@ def get_campaign_comment_rank(user_subscription_id):
     ])
     l = list(cursor)
     return l[0].get('campaign_rank',[]) if l else []
+
+def get_campaign_complete_sales(campaign_id):
+
+    cursor=db.api_order.aggregate([
+        {"$match":{"campaign_id":campaign_id,'status': 'complete'}},
+        {
+            "$group":
+                {
+                "_id":None,
+                "campaign_sales": { "$sum": "$subtotal" },
+                }
+        },
+        {"$project":{"_id":0,"campaign_sales":1}},
+    ])
+
+    l = list(cursor)
+    return l[0].get('campaign_sales',0) if l else 0
+
+
+def get_total_order_complete_proceed(user_subscription_id):
+
+    cursor=db.api_user_subscription.aggregate([
+        {"$match":{"id":user_subscription_id}},
+        {
+            "$lookup": {
+                "from": "api_campaign","localField": "id","foreignField": "user_subscription_id","as": "campaigns",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {
+                        "$lookup": {
+                            "from": "api_order","localField": "id","foreignField": "campaign_id","as": "orders",
+                            "pipeline":[
+                                {"$match":{"id":{"$ne":None}}},
+                                {"$project":{"_id":0,
+                                "complete": {  "$cond": [ { "$eq": ["$status", "complete" ] }, 1, 0]},
+                                "review": { "$cond": [ { "$eq": [ "$status", "review" ] }, 1, 0]}
+                                }},
+                            ]
+                        },
+                    },
+                    {"$project":{"_id":0,"id":1,"orders":1,"campaign_complete_count":{"$sum":"$orders.complete"},"campaign_review_count":{"$sum":"$orders.review"}}}
+                ]
+            },
+        },
+        {"$project":{"_id":0,
+        # "campaigns":1,
+        "total_complete_count":{"$sum":"$campaigns.campaign_complete_count"},
+        "total_review_count":{"$sum":"$campaigns.campaign_review_count"}
+        }}
+    ])
+    l = list(cursor)
+    return l[0].get('total_complete_count',0) if l else 0,\
+        l[0].get('total_review_count',0) if l else 0
+
+def get_campaign_order_complete_proceed(campaign_id):
+
+    cursor=db.api_campaign.aggregate([
+        {"$match":{"id":campaign_id}},
+        {
+            "$lookup": {
+                "from": "api_order","localField": "id","foreignField": "campaign_id","as": "orders",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {"$project":{"_id":0,
+                    "complete": {  "$cond": [ { "$eq": ["$status", "complete" ] }, 1, 0]},
+                    "review": { "$cond": [ { "$eq": [ "$status", "review" ] }, 1, 0]}
+                    }},
+                ]
+            },
+        },
+        {"$project":{"_id":0,
+        "orders":1,
+        "complete_count":{"$sum":"$orders.complete"},
+        "review_count":{"$sum":"$orders.review"}
+        }}
+    ])
+    l = list(cursor)
+    return l[0].get('complete_count',0) if l else 0,\
+        l[0].get('review_count',0) if l else 0
+
+
+def get_total_pre_order_count(user_subscription_id):
+
+    cursor=db.api_user_subscription.aggregate([
+        {"$match":{"id":user_subscription_id}},
+        {
+            "$lookup": {
+                "from": "api_campaign","localField": "id","foreignField": "user_subscription_id","as": "campaigns",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {
+                        "$lookup": {
+                            "from": "api_pre_order","localField": "id","foreignField": "campaign_id","as": "pre_orders",
+                            "pipeline":[
+                                {"$match":{"id":{"$ne":None},"subtotal":{"$ne":0}}},
+                                {"$project":{"_id":0, "count":{"$sum":1}}}
+                            ]
+                        },
+                    },
+                    {"$project":{"_id":0, "id":1,"pre_orders":1, "campaign_count":{"$sum":"$pre_orders.count"}}}
+                ]
+            },
+        },
+        {"$project":{"_id":0,
+        # "campaigns":1,
+        "total_count":{"$sum":"$campaigns.campaign_count"}
+        }}
+    ])
+    l = list(cursor)
+    return l[0].get('total_count',0) if l else 0
+
+
+def get_total_average_sales(user_subscription_id):
+    cursor=db.api_user_subscription.aggregate([
+        {"$match":{"id":user_subscription_id}},
+        {
+            "$lookup": {
+                "from": "api_campaign","localField": "id","foreignField": "user_subscription_id","as": "campaigns",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {
+                        "$lookup": {
+                            "from": "api_order","localField": "id","foreignField": "campaign_id","as": "orders",
+                            "pipeline":[
+                                {"$match":{"id":{"$ne":None}}},
+                                {"$project":{"_id":0,"id":1,"subtotal":1}},
+                            ]
+                        },
+                    },
+                    {"$project":{"_id":0,"id":1,
+                    "campaign_sales":{"$sum":"$orders.subtotal"},
+                    "orders":1}}
+                ]
+            },
+        },
+        {"$project":{"_id":0,
+        "average_sales":{"$avg":"$campaigns.campaign_sales"},
+        # "campaigns":1
+        }}
+    ])
+    l = list(cursor)
+    return l[0].get('average_sales',0) if l else 0
+
+
+def get_total_average_comment_count(user_subscription_id):
+    cursor=db.api_user_subscription.aggregate([
+        {"$match":{"id":user_subscription_id}},
+        {
+            "$lookup": {
+                "from": "api_campaign","localField": "id","foreignField": "user_subscription_id","as": "campaigns",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {
+                        "$lookup": {
+                            "from": "api_campaign_comment","localField": "id","foreignField": "campaign_id","as": "comments",
+                            "pipeline":[
+                                {"$match":{"id":{"$ne":None}}},
+                                {"$project":{"_id":0,"id":1,"count":{"$sum":1}}},
+                            ]
+                        },
+                    },
+                    {"$project":{"_id":0,"id":1,
+                    "campaign_comments_count":{"$sum":"$comments.count"},
+                    "orders":1}}
+                ]
+            },
+        },
+        {"$project":{"_id":0,
+        "average_comment_count":{"$avg":"$campaigns.campaign_comments_count"},
+        # "campaigns":1
+        }}
+    ])
+    l = list(cursor)
+    return l[0].get('average_comment_count',0) if l else 0
+
+
+def get_campaign_merge_order_list(campaign_id):
+
+    cursor=db.api_campaign.aggregate([
+        {"$match":{"id":campaign_id}},
+        {
+            "$lookup": {
+                "from": "api_order","localField": "id","foreignField": "campaign_id","as": "orders",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {"$addFields": { "type": "order","total_item": {"$size": { "$objectToArray": "$products"}}}},
+                ]
+            },
+        },
+        {
+            "$lookup": {
+                "from": "api_pre_order","localField": "id","foreignField": "campaign_id","as": "pre_orders",
+                "pipeline":[
+                    {"$match":{"id":{"$ne":None}}},
+                    {"$addFields": { "type": "pre_order","total_item": {"$size": { "$objectToArray": "$products"}}}},
+                ]
+            },
+        },
+        {"$project":{"_id":0,"data":{"$concatArrays":["$orders","$pre_orders"]}}},
+        { "$unwind": "$data" },
+        { "$sort" : { "data.created_at" : -1 } },
+        {"$group":{
+            "_id": {
+                "id": "$data.id",
+                "type": "$data.type"
+            },
+            "id":{"$first":"$data.id"},
+            "platform":{"$first":"$data.platform"},
+            "customer_name":{"$first":"$data.customer_name"},
+            "customer_img":{"$first":"$data.customer_img"},
+            "total_item":{"$first":"$data.total_item"},
+            "subtotal":{"$first":"$data.subtotal"},
+            "total":{"$first":"$data.total"},
+            "payment_method":{"$first":"$data.payment_method"},
+            "status":{"$first":"$data.status"},
+            "type":{"$first":"$data.type"}
+        }},
+        {"$project":{"_id":0,}}
+    ])
+    l = list(cursor)
+    # print(l)
+    return l
--- backend/api/nlp/classify.py
+++ backend/api/nlp/classify.py
@@ -4,19 +4,21 @@ from django.conf import settings
 import numpy as np
 
 def classify_comment_v1(texts,threshold=0.7):
-    categories = np.array(['neutro','payment','delivery'])
+    try:
+        categories = np.array(['neutro','payment','delivery'])
 
-    data={
-            "instances":texts
-        }
-    code,response = TextClassifierApiCaller("v1/models/lss_comment_classification_v1:predict", data=data).post()
+        data={
+                "instances":texts
+            }
+        code,response = TextClassifierApiCaller("v1/models/lss_comment_classification_v1:predict", data=data).post()
 
-    if code != 200:
-        return None
+        if code != 200:
+            return None
 
-    predictions = np.array(response.get("predictions"))
-    max_index = np.argmax(predictions,axis=1)+1
-    threshold_filter = np.sum((predictions>threshold),axis=1)
-    index = max_index*threshold_filter
-
+        predictions = np.array(response.get("predictions"))
+        max_index = np.argmax(predictions,axis=1)+1
+        threshold_filter = np.sum((predictions>threshold),axis=1)
+        index = max_index*threshold_filter
+    except Exception:
+        return []
     return list(categories[index])
