7520cf5b2228fbffe0019b540b31fdbf738685fb
asdf50003206 <asdf500032061@gmail.com>
2022-04-29 15:26:22 +0800

    update comment model

--- api/models/campaign/campaign_comment.py
+++ api/models/campaign/campaign_comment.py
@@ -11,32 +11,7 @@ class CampaignComment(models.Model):
     class Meta:
         db_table = 'api_campaign_comment'
 
-    id = models.IntegerField(unique=False)  #
-    # STATUS_CHOICES = [
-    #     (0, 'New'),
-    #     (1, 'Processed'),
-    # ]
-
-    # campaign = models.ForeignKey(
-    #     Campaign, on_delete=models.CASCADE, related_name='comments')
-
-    # comment_id = models.CharField(max_length=255, null=True, blank=True)
-    # message = models.TextField(null=True, blank=True, default=None)
-    # commented_at = models.IntegerField(
-    #     null=True, blank=True, default=time.time)
-    # customer_id = models.CharField(max_length=255, null=True, blank=True)
-    # customer_name = models.CharField(max_length=255, null=True, blank=True)
-    # image = models.CharField(max_length=512, null=True, blank=True)
-
-    # platform = models.CharField(max_length=255, blank=True,
-    #                             choices=settings.SUPPORTED_PLATFORMS, default='n/a')
-    # type = models.CharField(max_length=255, null=True, blank=True)
-    # status = models.IntegerField(blank=True,
-    #                              choices=STATUS_CHOICES, default=0)
-    # created_at = models.DateTimeField(auto_now_add=True)
-
-    # meta = models.JSONField(null=True, blank=True, default=dict)
-
+    id = models.IntegerField(primary_key=True, unique=False)  
 
 class CampaignCommentSerializer(serializers.ModelSerializer):
     class Meta:
