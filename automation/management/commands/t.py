adac1594a776f9cb27e6f3f90b5fd3b785e6d0ef
asdf50003206 <asdf500032061@gmail.com>
2022-05-05 12:24:49 +0800

    test sendinblue

--- automation/management/commands/t.py
+++ automation/management/commands/t.py
@@ -46,78 +46,7 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        # self.test_mongodb_query()
-        # self.test_send_email()
-        # self.test_user_plan()
-        # ret = api_twitch_get_access_token()
-        # print (ret)
-        # if settings.GCP_API_LOADBALANCER_URL == "https://sb.liveshowseller.ph":
-        #     self.modify_database()
-        self.test_send_email()
-
-
-    # def campaign_test(self):
-    #     cs = CampaignManager.get_active_campaigns()
-    #     print(cs)
-    #     cs = CampaignManager.get_ordering_campaigns()
-    #     print(cs)
-
-    # def cart_product_manager_test(self):
-    #     campaign = Campaign.objects.get(id=1)
-    #     campaign_product = CampaignProduct.objects.get(id=1)
-
-    #     cart_product_request = CartManager.create_cart_product_request(
-    #         campaign, 'facebook', '3141324909312956', 'Liu Ian', {
-    #             campaign_product: 5,
-    #         }
-    #     )
-    #     cart_product_request = CartManager.process(cart_product_request)
-
-    # def lucky_draw_test(self):
-    #     c = Campaign.objects.get(id=1)
-    #     cp = CampaignProduct.objects.get(id=1)
-    #     prize_cp = CampaignProduct.objects.get(id=2)
-
-    #     # lucky_draw = CampaignLuckyDrawManager.process(
-    #     #     c, DrawFromCampaignLikesEvent(c), prize_cp, 1,
-    #     # )
-
-    #     # keyword='testtest'
-    #     # lucky_draw = CampaignLuckyDrawManager.process(
-    #     #     c, DrawFromCampaignCommentsEvent(c, keyword), prize_cp, 1,
-    #     # )
-
-    #     lucky_draw = CampaignLuckyDrawManager.process(
-    #         c, DrawFromCartProductsEvent(c, cp), prize_cp, 1,
-    #     )
-
-    #     print(lucky_draw.__dict__)
-
-    # def cart_product_test(self):
-    #     c = Campaign.objects.get(id=1)
-    #     cp = CampaignProduct.objects.get(id=1)
-    #     cps = cart_product.filter_cart_products(
-    #         c, cp, ('order_code', 'cart'), ('valid',))
-    #     print(c, cp, cps)
-
-    # def campagin_product_test(self):
-    #     cp = CampaignProduct.objects.get(id=1)
-    #     r = CampaignProductStatusProcessor.update_status(
-    #         cp, CampaignProductStatusProcessor.Event.DEACTIVATE)
-
-    # def i18n_test(self):
-    #     from backend.i18n.campaign_announcement import \
-    #         i18n_get_campaign_announcement_lucky_draw_winner
-    #     customer_name = 'John'
-    #     product_name = 'Phone'
-    #     print(i18n_get_campaign_announcement_lucky_draw_winner(
-    #         customer_name, product_name))
-    #     print(i18n_get_campaign_announcement_lucky_draw_winner(
-    #         customer_name, product_name, lang='en'))
-    #     print(i18n_get_campaign_announcement_lucky_draw_winner(
-    #         customer_name, product_name, lang='zh-hant'))
-    #     print(i18n_get_campaign_announcement_lucky_draw_winner(
-    #         customer_name, product_name, lang='zh-hans'))
+        self.test_sendinblue()
 
 
     def modify_database(self):
@@ -282,3 +211,30 @@ class Command(BaseCommand):
         # print(SubscriptionPlan.__bases__.)
         print ([cls_attribute.__name__  for cls_attribute in SubscriptionPlan.__dict__.values() if type(cls_attribute)==type])
         # print([cls.__name__ for cls in SubscriptionPlan.__bases__])
+
+    def test_sendinblue(self):
+        API_KEY = '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'
+
+        import sib_api_v3_sdk
+        from sib_api_v3_sdk.rest import ApiException
+        import service.sendinblue as sib
+
+        configuration = sib_api_v3_sdk.Configuration()
+        configuration.api_key['api-key'] = API_KEY
+
+        api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+        to = [{"email":"alphatw22193@gmail.com","name":"Jane Doe"}]
+        # cc = [{"email":"example2@example2.com","name":"Janice Doe"}]
+        params = {"username":"User Name Params","test":"Test Params"}
+        send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+            template_id=sib.template.registration.TEST,
+            to=to, 
+            # cc=cc, 
+            params=params)
+
+        try:
+            api_response = api_instance.send_transac_email(send_smtp_email)
+            print(api_response)
+        except ApiException as e:
+            print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+        
