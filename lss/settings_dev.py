17efc4c62f51281138c323134b0c97bc47cabbbc
asdf50003206 <asdf500032061@gmail.com>
2022-05-11 14:38:45 +0800

    update google api key

--- lss/settings_dev.py
+++ lss/settings_dev.py
@@ -9,7 +9,7 @@ MONGODB_DATABASE_NAME = 'lss'
 
 
 # Live Show Seller info
-WEB_SERVER_URL = "https://v1login.liveshowseller.com"
+WEB_SERVER_URL = "https://liveshowseller.pagekite.com"
 
 SHOPPING_CART_URL = f"{WEB_SERVER_URL}/buyer/login_to_cart"
 
@@ -43,3 +43,7 @@ WEBPACK_LOADER = {
     'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
   }
 }
+
+# Google API credentials
+GOOGLE_OAUTH_CLIENT_ID_FOR_LIVESHOWSELLER = "715361967747-fvvrbaf18n412htlb92p8k6558kj4ugi.apps.googleusercontent.com"
+GOOGLE_OAUTH_CLIENT_SECRET_FOR_LIVESHOWSELLER = "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"
