4005d1c1213c5ded8d61b041ed8f00bacbb55405
asdf50003206 <asdf500032061@gmail.com>
2022-05-13 18:27:51 +0800

    redis to local machine

--- service/redis/redis.py
+++ service/redis/redis.py
@@ -2,17 +2,22 @@ from ._redis import redis_connection
 
 
 def increment(key):
-    count = redis_connection.get(key) if redis_connection.get(key) else 0
-    return redis_connection.set(key, count+1)
+    byte = redis_connection.get(key)
+    count = int(byte) if byte else 0
+    return redis_connection.set(key, str(count+1))
 
 def set(key, value):
     return redis_connection.set(key, value)
+
+def set_count(key, count):
+    return redis_connection.set(key, str(count))
 
 def get(key):
     return redis_connection.get(key)
 
 def get_count(key):
-    return redis_connection.get(key) if redis_connection.get(key) else 0
-    
+    byte = redis_connection.get(key)
+    return int(byte) if byte else 0
+
 def delete(key):
     return redis_connection.delete(key)
