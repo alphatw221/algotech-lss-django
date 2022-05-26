70b59d4e158425d3517cba7c3faf4a0856d2d06d
Nick LIen <nicklien@accoladeglobal.net>
2022-05-26 14:08:35 +0800

    fix: reset password email title not translated

--- backend/i18n/email/subject.py
+++ backend/i18n/email/subject.py
@@ -1,3 +1,4 @@
+from django.utils import translation
 from django.utils.translation import gettext as _
 
 
@@ -5,10 +6,12 @@ from django.utils.translation import gettext as _
 
 
 def i18n_get_reset_password_mail_subject(lang=None):
-    return  _('EMAIL/PASSWORD_RESET_LINK/SUBJECT')
+    with translation.override(lang):
+        return  _('EMAIL/PASSWORD_RESET_LINK/SUBJECT')
 
 def i18n_get_reset_password_success_mail_subject(lang=None):
-    return  _('EMAIL/PASSWORD_RESET_SUCCESS/SUBJECT')
+    with translation.override(lang):
+        return  _('EMAIL/PASSWORD_RESET_SUCCESS/SUBJECT')
 
 
 def get_foo_subject():
