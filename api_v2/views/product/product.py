d4d6ed151bea37bb11c1ddbe29ce489fe2e852b7
asdf50003206 <asdf500032061@gmail.com>
2022-05-30 14:43:20 +0800

    update product category

--- api_v2/views/product/product.py
+++ api_v2/views/product/product.py
@@ -1,7 +1,3 @@
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile
-
-from rest_framework.parsers import MultiPartParser
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
@@ -9,12 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from api import models
-from api import utils
-from api.utils.error_handle.error_handler.api_error_handler import api_error_handler
-
-from backend.pymongo.mongodb import db
-
-import json
+import lib 
 
 class ProductPagination(PageNumberPagination):
     page_query_param = 'page'
@@ -27,20 +18,23 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = models.product.product.ProductSerializer
     pagination_class = ProductPagination
 
-    @action(detail=False, methods=['GET'], url_path=r'list', permission_classes=(IsAuthenticated,))
-    @api_error_handler
+    @action(detail=False, methods=['GET'], url_path=r'search', permission_classes=(IsAuthenticated,))
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
     def list_product(self, request):
-        api_user, search_column, keyword, product_status = utils.common.common.getparams(request, ("search_column", "keyword", "product_status"), with_user=True, seller=True)
-        user_subscription = utils.common.verify.Verify.get_user_subscription_from_api_user(api_user)
-        
-        kwargs = {}
+        api_user, search_column, keyword, product_status = \
+            lib.util.getter.getparams(request, ("search_column", "keyword", "product_status"), with_user=True, seller=True)
+
+        user_subscription = \
+            lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
+
+        kwargs = {'status':product_status if product_status else 'enabled'}
         if (search_column in ["", None]) and (keyword not in [None, ""]):
-            raise utils.common.verify.ApiVerifyError("search_column field can not be empty when keyword has value")
+            raise lib.error_handle.error.api_error.ApiVerifyError("search_column field can not be empty when keyword has value")
         if (search_column not in ['undefined', '']) and (keyword not in ['undefined', '', None]):
             kwargs = { search_column + '__icontains': keyword }
-        kwargs['status'] = product_status
 
-        queryset = user_subscription.products.all().order_by('id').filter(**kwargs)
+
+        queryset = user_subscription.products.filter(**kwargs)
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
@@ -53,29 +47,36 @@ class ProductViewSet(viewsets.ModelViewSet):
         return Response(data, status=status.HTTP_200_OK)
 
 
-    @action(detail=False, methods=['GET'], url_path=r'list/category', permission_classes=(IsAuthenticated,))
-    @api_error_handler
+    @action(detail=False, methods=['GET'], url_path=r'categories', permission_classes=(IsAuthenticated,))
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
     def list_category(self, request):
-        api_user, = utils.common.common.getparams(request, (), with_user=True, seller=True)
-        user_subscription = utils.common.verify.Verify.get_user_subscription_from_api_user(api_user)
+        api_user, = lib.util.getter.getparams(request, (), with_user=True, seller=True)
 
-        categories_list = user_subscription.meta.get('categories', [])
-        return Response(categories_list, status=status.HTTP_200_OK)
+        user_subscription = lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
+
+        product_categories = user_subscription.meta.get('product_categories', [])
+        return Response(product_categories, status=status.HTTP_200_OK)
     
 
-    @action(detail=False, methods=['GET'], url_path=r'create/category', permission_classes=(IsAuthenticated,))
-    @api_error_handler
+    @action(detail=False, methods=['POST'], url_path=r'category/create', permission_classes=(IsAuthenticated,))
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
     def create_category(self, request):
-        api_user, category_name = utils.common.common.getparams(request, ("category_name", ), with_user=True, seller=True)
-        user_subscription = utils.common.verify.Verify.get_user_subscription_from_api_user(api_user)
 
-        categories_list = user_subscription.meta.get('categories', [])
-        categories_list.append(category_name)
-        user_subscription.meta['categories'] = categories_list
+        api_user = lib.util.verify.Verify.get_seller_user(request)
+        category_name, = lib.util.getter.getdata(request,('category_name',),required=True)
+        user_subscription = lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
+        product_categories = user_subscription.meta.get('product_categories',[])
+        if not category_name:
+            raise lib.error_handle.error.api_error.ApiVerifyError("invalid category name")
+        if category_name in product_categories:
+            raise lib.error_handle.error.api_error.ApiVerifyError("category already exists")
+        product_categories.append(category_name)
+        user_subscription.meta['product_categories'] = product_categories
         user_subscription.save()
 
-        return Response(categories_list, status=status.HTTP_200_OK)
+        return Response(product_categories, status=status.HTTP_200_OK)
     
+
 
     # @action(detail=True, methods=['PUT'], url_path=r'update_product', parser_classes=(MultiPartParser,), permission_classes=(IsAuthenticated,))
     # @api_error_handler
