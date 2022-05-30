280cab8976faf4eae8885a9ab497e245b403a20b
Nick LIen <nicklien@accoladeglobal.net>
2022-05-30 14:43:37 +0800

    add deal, promotion_cdoe model

---
+++ api/models/user/promotion_code.py
@@ -0,0 +1,27 @@
+
+from statistics import mode
+from unicodedata import name
+from django.contrib import admin
+from django.contrib.auth.models import User as AuthUser
+from djongo import models
+from api.models.user.user import User
+from api.models.user.user_subscription import UserSubscription
+
+class PromotionCode(models.Model):
+    class Meta:
+        db_table = 'api_promotion_code'
+        
+    name = models.CharField(max_length=255, null=True, blank=True)
+    code = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(
+        User,  null=True, on_delete=models.SET_NULL, related_name='promotion_code')
+    user_subscription = models.ForeignKey(
+        UserSubscription,  null=True, on_delete=models.SET_NULL, related_name='promotion_code')
+    used_at = models.DateTimeField()
+    meta = models.JSONField(null=True, blank=True, default=dict)
+    
+    def __str__(self):
+        return f'{str(self.name)} {str(self.code)}'
+    
+
+
