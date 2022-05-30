4cc0aa0a377ef9d1ac64f3fe9983070ce79bf958
asdf50003206 <asdf500032061@gmail.com>
2022-05-30 12:36:44 +0800

    update max order amount check rule

---
+++ api/rule/check_rule/product_check_rule.py
@@ -0,0 +1,14 @@
+import lib
+
+class ProductCheckRule():
+
+    @staticmethod
+    def is_max_order_amount_less_than_qty(**kwargs):
+        product_data = kwargs.get('product_data')
+        max_order_amount = product_data.get('max_order_amount')
+        if not  max_order_amount:
+            pass
+        if max_order_amount > product_data.get('qty',0):
+            raise lib.error_handle.error.api_error.ApiVerifyError('max order amount should be less than stock amount')
+
+  
