1926b2b85c08e4231d9ca8a8441cb641e3abb6d9
asdf50003206 <asdf500032061@gmail.com>
2022-06-02 13:47:00 +0800

    update order upload receipt

--- api_v2/views/order/order.py
+++ api_v2/views/order/order.py
@@ -2,6 +2,11 @@ from rest_framework.response import Response
 from rest_framework import viewsets,status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action, permission_classes
+from rest_framework.parsers import MultiPartParser
+
+from django.core.files.storage import default_storage
+from django.conf import settings
+from django.core.files.base import ContentFile
 
 from api import models
 from api.utils.common.order_helper import PreOrderHelper
@@ -12,8 +17,40 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = models.order.order.Order.objects.all().order_by('id')
 
     @action(detail=True, methods=['GET'], url_path=r'buyer/retrieve', permission_classes=(IsAuthenticated,))
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
     def buyer_retrieve_order(self, request, pk=None):
         api_user = lib.util.verify.Verify.get_customer_user(request)
         order = lib.util.verify.Verify.get_order_by_api_user(api_user, pk)
 
         return Response(models.order.order.OrderSerializer(order).data, status=status.HTTP_200_OK)
+
+
+    @action(detail=True, methods=['PUT'], url_path=r'buyer/receipt/upload', parser_classes=(MultiPartParser,), permission_classes=(IsAuthenticated,))
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
+    def buyer_upload_receipt(self, request, pk):
+
+        api_user = lib.util.verify.Verify.get_customer_user(request)
+        order = lib.util.verify.Verify.get_order_by_api_user(api_user, pk)
+
+        last_five_digit, image, =lib.util.getter.getdata(request,('last_five_digit', 'image'), required=False)
+        
+        
+        if image and image != "undefined":
+            image_path = default_storage.save(
+                f'campaign/{order.campaign.id}/order/{order.id}/receipt/{image.name}', 
+                ContentFile(image.read())
+            )
+            order.meta["receipt_image"] = settings.GS_URL + image_path
+
+        if last_five_digit:
+            order.meta["last_five_digit"] = last_five_digit
+
+        order.payment_method = "Direct Payment"
+        order.status = "complete"
+        order.save()
+        # send_email(order_id)
+        # shop, order, campaign = confirmation_email_info(order_id)
+        # sib_service.transaction_email.OrderConfirmationEmail(shop=shop, order=order, campaign=campaign, to=[order.get('shipping_email')], cc=[]).send()
+
+        return Response(models.order.order.OrderSerializer(order).data, status=status.HTTP_200_OK)
+
