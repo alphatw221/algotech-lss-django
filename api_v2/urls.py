0d358f76985a0b12f35417da9073b5bcb5a35246
asdf50003206 <asdf500032061@gmail.com>
2022-06-02 13:24:00 +0800

    update order retrieve

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -6,6 +6,7 @@ from api_v2.views.campaign import campaign
 from api_v2.views.user import user
 from api_v2.views.order import pre_order
 from api_v2.views.order import order_product
+from api_v2.views.order import order
 
 router = routers.DefaultRouter()
 router.register(r'product', product.ProductViewSet)
@@ -13,6 +14,7 @@ router.register(r'campaign',campaign.CampaignViewSet)
 router.register(r'user', user.UserViewSet)
 router.register(r'pre_order', pre_order.PreOrderViewSet)
 router.register(r'order_product', order_product.OrderProductViewSet)
+router.register(r'order', order.OrderViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
