47518334f3f18e9275922c9844de58a5ed264186
Nick LIen <nicklien@accoladeglobal.net>
2022-06-14 15:52:40 +0800

    fix websocket function

--- service/channels/campaign.py
+++ service/channels/campaign.py
@@ -4,13 +4,10 @@ from channels.layers import get_channel_layer
 
 
 def send_comment_data(campaign_id, data):
-    del data['_id']
     async_to_sync(get_channel_layer().group_send)(f"campaign_{campaign_id}", {"type": "comment_data","data":data})
     
 def send_order_data(campaign_id, data):
-    del data['_id']
     async_to_sync(get_channel_layer().group_send)(f"campaign_{campaign_id}", {"type": "order_data","data":data})
     
 def send_product_data(campaign_id, data):
-    del data['_id']
     async_to_sync(get_channel_layer().group_send)(f"campaign_{campaign_id}", {"type": "product_data","data":data})
