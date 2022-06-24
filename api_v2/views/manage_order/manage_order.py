126d2eb4ae0983ec70e8e6a7b9a4a833d8739879
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-06-24 10:15:57 +0800

    manage order v2

--- api_v2/views/manage_order/manage_order.py
+++ api_v2/views/manage_order/manage_order.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action, permission_classes
 
 from api import models
 from api.utils.common.order_helper import PreOrderHelper
-from api.utils.advance_query.dashboard import get_campaign_merge_order_list
+from api.utils.advance_query.dashboard import get_campaign_merge_order_list_v2
 from bson.json_util import loads, dumps
 import lib   
     
@@ -15,20 +15,25 @@ class DashboardViewSet(viewsets.ModelViewSet):
     queryset = models.order.order.Order.objects.all().order_by('id')
     filterset_fields = []
     
-    @action(detail=False, methods=['GET'], url_path=r'order_list')
+    @action(detail=False, methods=['POST'], url_path=r'order_list')
     @lib.error_handle.error_handler.api_error_handler.api_error_handler
     def get_merge_order_list(self, request):
 
         api_user, campaign_id, search, page, page_size= lib.util.getter.getparams(request, ( 'campaign_id', 'search', 'page', 'page_size'),with_user=True, seller=True)
-
+        f_payment,f_delivery,f_platform = lib.util.getter.getdata(request,('payment','delivery','platform'))
+        print(f_payment,f_delivery,f_platform)
         user_subscription = lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
         campaign = lib.util.verify.Verify.get_campaign_from_user_subscription(user_subscription,campaign_id)
-        merge_list = get_campaign_merge_order_list(campaign.id, search, page, page_size)
-
+        merge_list = get_campaign_merge_order_list_v2(campaign.id, search,f_payment,f_delivery,f_platform)
+        
+        page,page_size = int(page),int(page_size)
+        skip = (page-1)*page_size
+        if len(merge_list) >= page_size:
+            merge_list = merge_list[skip:skip+page_size]
         merge_list_str = dumps(merge_list)
         merge_list_json = loads(merge_list_str)
 
-        return Response(merge_list_json, status=status.HTTP_200_OK)
+        return Response({'count':len(merge_list),'data':merge_list_json}, status=status.HTTP_200_OK)
     
     @action(detail=False, methods=['GET'], url_path=r'edit_allow_checkout')
     @lib.error_handle.error_handler.api_error_handler.api_error_handler
