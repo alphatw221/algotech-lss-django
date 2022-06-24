98ea8e57251fad165534cb469e7d0ad1d19062f9
asdf50003206 <asdf500032061@gmail.com>
2022-06-24 18:47:20 +0800

    update auto response

--- database/lss/__init__.py
+++ database/lss/__init__.py
@@ -1,1 +1,1 @@
-from . import order, pre_order, util, lucky_draw
+from . import order, pre_order, util, lucky_draw, campaign,  user_subscription
---
+++ database/lss/user_subscription.py
@@ -0,0 +1,57 @@
+from ._config import db
+from ._config import Collection
+
+__collection = db.api_user_subscription
+
+
+class UserSubscription(Collection):
+
+    _collection = db.api_user_subscription
+
+
+from pprint import pprint
+def get_user_subscription_campaign_count():
+
+    cursor=db.api_user_subscription.aggregate([
+        {"$match":{"id":{"$ne":None}}},
+
+        {
+            "$lookup": {
+                "from": "api_user_subscription_facebook_pages",
+                "as": "facebook_pages", # "localField": "id","foreignField": "user_subscription_id",
+                'let': {'id': "$id" },
+                "pipeline":[
+                    {"$match":{
+                        '$expr': { '$eq': ["$$id", "$user_subscription_id"] },
+                        "id":{"$ne":None}}
+                     },
+                    {"$project":{"_id":0,"count":{"$sum":1}}},
+                ]
+            },
+        },
+
+        
+        {"$project":{"_id":0,"facebook_page_count":{"$sum":"$facebook_pages.count"}, "name":1, "id":1 }},
+
+        # {
+        #     "$lookup": {
+        #         "from": "api_campaign",
+        #         "as": "campaigns", # "localField": "id","foreignField": "user_subscription_id",
+        #         'let': {'id': "$id" },
+        #         "pipeline":[
+        #             {"$match":{
+        #                 '$expr': { '$eq': ["$$id", "$user_subscription_id"] },
+        #                 "id":{"$ne":None}}
+        #              },
+        #             {"$project":{"_id":0,"count":{"$sum":1}}},
+        #         ]
+        #     },
+        # },
+        # {"$project":{"_id":0,
+        #     "name":1,
+        #     "id":1,
+        #     "campaign_count":{"$sum":"$campaigns.count"},
+        # }},
+    ])
+    l = list(cursor)
+    pprint(l)
