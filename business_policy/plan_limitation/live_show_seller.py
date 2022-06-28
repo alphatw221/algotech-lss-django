8c01f8241f145e4594b36bbc498e0d7214f20042
Nick LIen <nicklien@accoladeglobal.net>
2022-06-28 17:43:16 +0800

    add

--- business_policy/plan_limitation/live_show_seller.py
+++ business_policy/plan_limitation/live_show_seller.py
@@ -3,29 +3,34 @@ trial = {
     'max_concurrent_live': 2,
     'channel_limit': 1,
     'products_limit': 10,
-    'orders_limit': 100
+    'orders_limit': 100,
+    'social_meida_connect': 1
 }
 lite = {
     'max_concurrent_live': 2,
     'channel_limit': 1,
     'products_limit': 30,
-    'orders_limit': 300
+    'orders_limit': 300,
+    'social_meida_connect': 1
 }
 standard = {
     'max_concurrent_live': 4,
     'channel_limit': 2,
     'products_limit': 100,
-    'orders_limit': 1000
+    'orders_limit': 1000,
+    'social_meida_connect': 2
 }
 premium = {
     'max_concurrent_live': 6,
     'channel_limit': 3,
     'products_limit': 300,
-    'orders_limit': 5000
+    'orders_limit': 5000,
+    'social_meida_connect': 3
 }
 dealer = {
     'max_concurrent_live': 100,
     'channel_limit': 30,
     'products_limit': 3000,
-    'orders_limit': 500000
+    'orders_limit': 500000,
+    'social_meida_connect': 3
 } 
