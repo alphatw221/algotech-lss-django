ce674b4f867a9b312deec2357fe435d9819635a7
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-06-29 18:18:16 +0800

    update api path

--- api_v2/views/campaign/campaign.py
+++ api_v2/views/campaign/campaign.py
@@ -341,3 +341,18 @@ class CampaignViewSet(viewsets.ModelViewSet):
         
 
         return Response(data, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=['GET'], url_path=r'edit_allow_checkout')
+    @lib.error_handle.error_handler.api_error_handler.api_error_handler
+    def campaign_edit_allow_checkout(self, request):
+
+        api_user, campaign_id, _status= lib.util.getter.getparams(request, ('campaign_id', 'status'))
+
+        user_subscription = lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
+        campaign = lib.util.verify.Verify.get_campaign_from_user_subscription(user_subscription,campaign_id)
+
+        campaign.meta['allow_checkout']=1 if int(_status) else 0
+        campaign.save()
+
+        return Response({"allow_checkout":campaign.meta['allow_checkout']
+                         }, status=status.HTTP_200_OK)
