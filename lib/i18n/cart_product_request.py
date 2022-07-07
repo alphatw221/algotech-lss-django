b03fae21ee4cdb9de95dea263d2f7a4fd9537bf7
asdf50003206 <asdf500032061@gmail.com>
2022-07-07 12:00:50 +0800

    update pre_order request state enmu

--- lib/i18n/cart_product_request.py
+++ lib/i18n/cart_product_request.py
@@ -3,6 +3,7 @@ from ._i18n import lang_translate_default_en
 from django.conf import settings
 from django.utils.translation import ugettext as _
 import lib
+from..helper.order_helper import RequestState
 
 @lang_translate_default_en
 def get_request_response(state, api_campaign_product, qty, lang=None):
@@ -24,27 +25,27 @@ def get_additional_text(pre_order, lang=None):
 
 def get_response_content(state, api_campaign_product, qty):
 
-    if state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_EXCEED_MAX_ORDER_AMOUNT:
+    if state == RequestState.INVALID_EXCEED_MAX_ORDER_AMOUNT:
         return _('INVALID_EXCEED_MAX_ORDER_AMOUNT{order_code}{max_order_amount}').format(
             order_code=api_campaign_product['order_code'],
             max_order_amount=api_campaign_product['max_order_amount'],
         )
     else:
-        if state == lib.helper.order_helper.PreOrderHelper.RequestState.ADDED:
+        if state == RequestState.ADDED:
             result = _('ADDED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.UPDATED:
+        elif state == RequestState.UPDATED:
             result = _('UPDATED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.DELETED:
+        elif state == RequestState.DELETED:
             result = _('DELETED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.INSUFFICIENT_INV:
+        elif state == RequestState.INSUFFICIENT_INV:
             result = _('INSUFFICIENT_INV')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_PRODUCT_NOT_ACTIVATED:
+        elif state == RequestState.INVALID_PRODUCT_NOT_ACTIVATED:
             result = _('INVALID_PRODUCT_NOT_ACTIVATED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_REMOVE_NOT_ALLOWED:
+        elif state == RequestState.INVALID_REMOVE_NOT_ALLOWED:
             result = _('INVALID_REMOVE_NOT_ALLOWED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_EDIT_NOT_ALLOWED:
+        elif state == RequestState.INVALID_EDIT_NOT_ALLOWED:
             result = _('INVALID_EDIT_NOT_ALLOWED')
-        elif state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_ADD_ZERO_QTY:
+        elif state == RequestState.INVALID_ADD_ZERO_QTY:
             result = _('INVALID_ADD_ZERO_QTY')
         else:
             result = _('N/A')
@@ -59,7 +60,7 @@ def get_response_content(state, api_campaign_product, qty):
 def get_items_info(request):
     items_info = []
     for item in request.get_items():
-        if item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_EXCEED_MAX_ORDER_AMOUNT:
+        if item.state == RequestState.INVALID_EXCEED_MAX_ORDER_AMOUNT:
             items_info.append(
                 _('INVALID_EXCEED_MAX_ORDER_AMOUNT{order_code}{max_order_amount}').format(
                     order_code=item.campaign_product.order_code,
@@ -67,21 +68,21 @@ def get_items_info(request):
                 )
             )
         else:
-            if item.state == lib.helper.order_helper.PreOrderHelper.RequestState.ADDED:
+            if item.state == RequestState.ADDED:
                 result = _('ADDED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.UPDATED:
+            elif item.state == RequestState.UPDATED:
                 result = _('UPDATED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.DELETED:
+            elif item.state == RequestState.DELETED:
                 result = _('DELETED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INSUFFICIENT_INV:
+            elif item.state == RequestState.INSUFFICIENT_INV:
                 result = _('INSUFFICIENT_INV')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_PRODUCT_NOT_ACTIVATED:
+            elif item.state == RequestState.INVALID_PRODUCT_NOT_ACTIVATED:
                 result = _('INVALID_PRODUCT_NOT_ACTIVATED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_REMOVE_NOT_ALLOWED:
+            elif item.state == RequestState.INVALID_REMOVE_NOT_ALLOWED:
                 result = _('INVALID_REMOVE_NOT_ALLOWED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_EDIT_NOT_ALLOWED:
+            elif item.state == RequestState.INVALID_EDIT_NOT_ALLOWED:
                 result = _('INVALID_EDIT_NOT_ALLOWED')
-            elif item.state == lib.helper.order_helper.PreOrderHelper.RequestState.INVALID_ADD_ZERO_QTY:
+            elif item.state == RequestState.INVALID_ADD_ZERO_QTY:
                 result = _('INVALID_ADD_ZERO_QTY')
             else:
                 result = _('N/A')
