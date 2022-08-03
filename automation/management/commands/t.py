1bd6c7fcf221eab437267ac6ff404eefcd72774b
asdf50003206 <asdf500032061@gmail.com>
2022-08-03 12:24:23 +0800

    update order_list

--- automation/management/commands/t.py
+++ automation/management/commands/t.py
@@ -51,7 +51,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # self.test_lucky_draw()
-        self.test_hitpay_verification()
+        self.test_mongo_aggr()
 
     def modify_database(self):
         from api.models.user.user_subscription import UserSubscription
@@ -264,94 +264,8 @@ class Command(BaseCommand):
     
     def test_mongo_aggr(self):
         from pprint import pprint 
-        # cursor = db.api_user_subscription.aggregate([
-        #     {'$match': {'id': 1}},
-        #     {'$lookup': {
-        #         'from': 'api_campaign',
-        #         'as': 'campaigns',
-        #         'let': { 'id': '$id' },
-        #         'pipeline': [
-        #             {'$match': {
-        #                 '$expr': { '$eq': [ '$$id', '$user_subscription_id' ] },
-        #                 'id': {'$ne': None}
-        #             }},
-        #             {'$project': { '_id': 0, 'id': 1 }}
-        #         ]
-        #     }}
-        # ])
-        # l = list(cursor)
-        # pprint (l)
-
-        # cursor = db.api_campaign.aggregate([
-        #     {'$match': {'id': 118}},
-        #     {'$lookup': {
-        #         'from': 'api_order',
-        #         'as': 'orders',
-        #         'let': { 'id': '$id' },
-        #         'pipeline': [
-        #             {'$match': {
-        #                 '$expr': { '$eq': [ '$$id', '$campaign_id'] }
-        #             }},
-        #             # {'$unwind': '$orders'},
-        #             {'$group': {
-        #                 '_id': '$customer_id',
-        #                 'customer_id': { '$first': '$customer_id'},
-        #                 'customer_name': { '$first': '$customer_name'},
-        #                 'customer_img': { '$first': '$customer_img'},
-        #                 'shipping_email': { '$first': '$shipping_email'},
-        #                 'shipping_phone': { '$first': '$shipping_phone'},
-        #                 'platform': { '$first': '$platform'},
-        #             }}
-        #         ],
-        #     }},
-        #     {'$unwind': '$orders'},
-        #     {'$project': {'_id': 0, 'orders': 1, }},
-        #     # {'$unwind': '$orders'}
-        # ])
-        # l = list(cursor)
-        # pprint (l)
-
-
-        cursor = db.api_user_subscription.aggregate([
-            {'$match': {'id': 1}},
-            {'$lookup': {
-                'from': 'api_campaign',
-                'as': 'campaigns',
-                'let': { 'id': '$id' },
-                'pipeline': [
-                    {'$match': {
-                        '$expr': { '$eq': [ '$$id', '$user_subscription_id' ] },
-                        'id': {'$ne': None}
-                    }},
-                    {'$project': { '_id': 0, 'id': 1 }},
-                    {'$lookup': {
-                        'from': 'api_order',
-                        'as': 'orders',
-                        'let': { 'id': '$id' },
-                        'pipeline': [
-                            {'$match': {
-                                '$expr': { '$eq': [ '$$id', '$campaign_id'] }
-                            }}
-                        ],
-                    }},
-                    {'$unwind': '$orders'},  
-                    {'$match': {'orders': {'$ne': []}}},
-                    {'$project': {'_id': 0, 'orders': 1}},
-                ]
-            }},
-            {'$unwind': '$campaigns'},  
-            {'$project': { '_id': 0, 'campaigns': 1 }},
-            {'$group': {
-                '_id': '$campaigns.orders.customer_id',
-                'customer_id': { '$first': '$campaigns.orders.customer_id'},
-                'customer_name': { '$first': '$campaigns.orders.customer_name'},
-                'customer_img': { '$first': '$campaigns.orders.customer_img'},
-                'shipping_email': { '$first': '$campaigns.orders.shipping_email'},
-                'shipping_phone': { '$first': '$campaigns.orders.shipping_phone'},
-                'platform': { '$first': '$campaigns.orders.platform'},
-            }},
-        ])
-        l = list(cursor)
+        import database
+        l = database.lss.campaign.get_merge_order_list_pagination(campaign_id=958, status='', search='', filter_payment=[], filter_delivery=[], filter_platform=[])
         pprint(l)
 
     def test_websocket(self):
--- database/lss/campaign.py
+++ database/lss/campaign.py
@@ -17,81 +17,29 @@ class Campaign(Collection):
 
 
 
-def get_merge_order_list_pagination(campaign_id, search:str, status:str, filter_payment:list, filter_delivery:list, filter_platform:list, page:int, page_size:int):
+def get_merge_order_list_pagination(campaign_id, search:str, status:str, filter_payment:list, filter_delivery:list, filter_platform:list, page:int=1, page_size:int=25):
 
-    # search and paginate by frontend by now
+    filter_query = {'$expr': { '$eq': ["$$id", "$campaign_id"] },"id":{"$ne":None} ,"products":{"$ne":{}}}
+
     if search not in ["",None,'undefined'] and search.isnumeric():
-        match_pipeline_query = {"$match":{"$or":[{"id":{"$eq":int(search)}}, {"customer_name":{"$regex":str(search),"$options": 'i'}}] }}
-    else:
-        match_pipeline_query = {"$match":{"id":{"$ne":None} }}
-    
+        filter_query["$or"]=[{"id":{"$eq":int(search)}}, {"customer_name":{"$regex":str(search),"$options": 'i'}}]
 
     if status == models.order.order.STATUS_REVIEW:
-        status_match_pipeline_query = {"$match":{"id":{"$ne":None},"status":{"$eq":models.order.order.STATUS_REVIEW} }}
+        filter_query['status']={"$in":[models.order.order.STATUS_REVIEW]} 
     elif status == models.order.order.STATUS_COMPLETE:
-        status_match_pipeline_query = {"$match":{"id":{"$ne":None},"status":{"$in":[models.order.order.STATUS_REVIEW,models.order.order.STATUS_SHIPPING_OUT]} }}
-    else:
-        status_match_pipeline_query = {"$match":{"id":{"$ne":None} }}
-        
+        filter_query['status']={"$in":[models.order.order.STATUS_COMPLETE,models.order.order.STATUS_SHIPPING_OUT]}
         
     if filter_payment not in [[],None]:
-        filter_payment_query = {"$match":{"id":{"$ne":None},"payment_method":{"$in": filter_payment} }}
-    else:
-        filter_payment_query = {"$match":{"id":{"$ne":None} }}
+        filter_query['payment_method']={"$in": filter_payment}
         
     if filter_platform not in [[],None]:
-        filter_platform_query = {"$match":{"id":{"$ne":None},"platform":{"$in": filter_platform} }}
-    else:
-        filter_platform_query = {"$match":{"id":{"$ne":None} }}
+        filter_query['platform']={"$in": filter_platform}
         
     if filter_delivery not in [[],None]:
-        filter_delivery_query = {"$match":{"id":{"$ne":None},"status":{"$in": filter_delivery} }}
-    else:
-        filter_delivery_query = {"$match":{"id":{"$ne":None} }}
-
-
-    count_query = [
-        {"$match":{"id":campaign_id}},
-        {
-            "$lookup": {
-                "from": "api_order","as": "orders",
-                'let': {'id': "$id" },
-                "pipeline":[
-                    {"$match":{
-                        '$expr': { '$eq': ["$$id", "$campaign_id"] },
-                        "id":{"$ne":None} }
-                    },
-                    status_match_pipeline_query,
-                    match_pipeline_query,
-                    filter_payment_query,
-                    filter_platform_query,
-                    filter_delivery_query,
-                    {"$project":{"_id":0, "count":{"$sum":1}}}
-                ]
-            },
-        },
-        {
-            "$lookup": {
-                "from": "api_pre_order","as": "pre_orders",
-                'let': {'id': "$id" },
-                "pipeline":[
-                    {"$match":{
-                        '$expr': { '$eq': ["$$id", "$campaign_id"] },
-                        "id":{"$ne":None} ,
-                        "subtotal":{"$ne":0}}
-                     },
-                    status_match_pipeline_query,
-                    match_pipeline_query,
-                    filter_payment_query,
-                    filter_platform_query,
-                    filter_delivery_query,
-                    {"$project":{"_id":0, "count":{"$sum":1}}}
-                ]
-            },
-        },
-        {"$project":{"_id":0, "id":1,"pre_orders":1, 'orders':1, "pre_order_count":{"$sum":"$pre_orders.count"},  "order_count":{"$sum":"$orders.count"}}}
-    ]
-
+        if filter_query.get('status',{}).get('$in'):
+            filter_query['status']['$in']+=filter_delivery
+        else:
+            filter_query['status']={"$in": filter_delivery}
 
     query = [
         {"$match":{"id":campaign_id}},
@@ -100,10 +48,7 @@ def get_merge_order_list_pagination(campaign_id, search:str, status:str, filter_
                 "from": "api_order","as": "orders",
                 'let': {'id': "$id" },
                 "pipeline":[
-                    {"$match":{
-                        '$expr': { '$eq': ["$$id", "$campaign_id"] },
-                        "id":{"$ne":None} }
-                     },
+                    {"$match":filter_query},
                     {"$addFields": { "type": "order","total_item": {"$size": { "$objectToArray": "$products"}}}},
                 ]
             },
@@ -113,16 +58,12 @@ def get_merge_order_list_pagination(campaign_id, search:str, status:str, filter_
                 "from": "api_pre_order","as": "pre_orders",
                 'let': {'id': "$id" },
                 "pipeline":[
-                    {"$match":{
-                        '$expr': { '$eq': ["$$id", "$campaign_id"] },
-                        "id":{"$ne":None} ,
-                        "subtotal":{"$ne":0}}
-                     },
+                    {"$match":filter_query},
                     {"$addFields": { "type": "pre_order","total_item": {"$size": { "$objectToArray": "$products"}}}},
                 ]
             },
         },
-        {"$project":{"_id":0,"data":{"$concatArrays":["$orders","$pre_orders"]}}},
+        {"$project":{"_id":0,"data":{"$concatArrays":["$orders","$pre_orders"]}} },
         { "$unwind": "$data" },
         { "$sort" : { "data.created_at" : -1 } },
         {"$group":{
@@ -144,27 +85,16 @@ def get_merge_order_list_pagination(campaign_id, search:str, status:str, filter_
             "type":{"$first":"$data.type"}
         }},
         {"$project":{"_id":0,}},
-        status_match_pipeline_query,
-        match_pipeline_query,
-        filter_payment_query,
-        filter_platform_query,
-        filter_delivery_query,
-        # { "$skip": (page-1)*page_size },    # search and paginate by frontend by now
-        # { "$limit": page_size }
     ]
-    page = int(page)
-    page_size = int(page_size)
 
-    count = db.api_campaign.aggregate(count_query)
-    pprint(count)
-    pprint(list(count))
-    query.append({ "$skip": (page-1)*page_size })
-    query.append({ "$limit": page_size })
     cursor=db.api_campaign.aggregate(query)
-    l = list(cursor)
 
-    merge_list_str = dumps(l)
-    merge_list_json = loads(merge_list_str)
+    skip = (page-1)*page_size
+    
+    bson = list(cursor)
+    total_count = len(bson)
+    bson = bson[skip:skip+page_size]
+    data_str = dumps(bson)
+    data_json = loads(data_str)    #TODO optimize # use skip limit  sum
 
-
-    return {'count':1,'data':merge_list_json}
+    return data_json, total_count
