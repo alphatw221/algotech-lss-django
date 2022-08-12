8d54479a89622c75439e55d2fbcd4f045fba3dc5
asdf50003206 <asdf500032061@gmail.com>
2022-08-12 14:37:39 +0800

    update job

--- automation/jobs/export_product.py
+++ automation/jobs/export_product.py
@@ -7,9 +7,10 @@ try:
 except Exception as e:
     pass
 
-# from .. import service as easy_store_service
+
+from plugins.easy_store import service as easy_store_service
 
 def export_product_job(user_subscription_id, credential):
     print('i am exporting product..')
 
-    # easy_store_service.channels.export_product.send_result_data(user_subscription_id,{'result':'complete'})
+    easy_store_service.channels.export_product.send_result_data(user_subscription_id,{'result':'complete'})
