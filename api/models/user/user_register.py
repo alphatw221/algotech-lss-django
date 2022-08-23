d13d125fa454d84ddf08302828fe14c0e816f412
e12mv2mgh <jeremychou@accoladeglobal.net>
2022-08-23 10:59:11 +0800

    update api_user_register table

--- api/models/user/user_register.py
+++ api/models/user/user_register.py
@@ -1,6 +1,8 @@
 from djongo import models
 import business_policy
 
+
+TIMEZONE_SG = 'Asia/Singapore'
 class UserRegister(models.Model):
     class Meta:
         db_table = 'api_user_register'
@@ -11,12 +13,12 @@ class UserRegister(models.Model):
     period = models.CharField(max_length=255,null=True,blank=True,default=business_policy.subscription.PERIOD_MONTH)
     created_at = models.DateTimeField(auto_now_add=True)
     timezone = models.CharField(
-        max_length=255, null=True, blank=True, default='Asia/Singapore')
+        max_length=255, null=True, blank=True, default=TIMEZONE_SG)
     phone = models.CharField(max_length=255, null=True, blank=True)
     email = models.CharField(max_length=255, null=True, blank=True)
     password = models.CharField(max_length=255, null=True, blank=True)
     target_country = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=255, blank=True, default='SG', null=True)
+    country = models.CharField(max_length=255, blank=True, default=business_policy.subscription.COUNTRY_SG, null=True)
     meta = models.JSONField(null=True, blank=True, default=dict)
     
     def __str__(self):
