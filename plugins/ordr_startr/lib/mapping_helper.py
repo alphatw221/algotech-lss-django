c357241e4cd3de417f19d1c68e8eaf8dc82b1593
asdf50003206 <asdf500032061@gmail.com>
2022-09-12 13:17:46 +0800

    update mapping

--- plugins/ordr_startr/lib/mapping_helper.py
+++ plugins/ordr_startr/lib/mapping_helper.py
@@ -9,7 +9,18 @@ PLUGIN_ORDR_STARTR = 'ordr_startr'
 class CampaignProduct:
     @staticmethod
     def get_internal_external_map(campaign):
-        return {str(product.id):{'id':product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id'),'order_code':product.order_code} for product in  campaign.products.all() if product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id')}
+
+        success, data, lock = database.lss_cache.campaign_product.leash_get_internal_external_map(campaign.id, PLUGIN_ORDR_STARTR)
+        if not success and lock :
+            with lock:
+                data = {str(product.id):{'id':product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id'),'order_code':product.order_code} for product in  campaign.products.all() if product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id')}
+                database.lss_cache.campaign_product.set_external_internal_map(campaign.id, PLUGIN_ORDR_STARTR, data)
+        elif not success:
+            data = {str(product.id):{'id':product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id'),'order_code':product.order_code} for product in  campaign.products.all() if product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id')}
+        return data
+
+
+        # return {str(product.id):{'id':product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id'),'order_code':product.order_code} for product in  campaign.products.all() if product.meta.get(PLUGIN_ORDR_STARTR,{}).get('id')}
 
     def get_external_internal_map(campaign):
 
