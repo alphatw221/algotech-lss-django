2e57dc67ffe77fe5d769cbf40bbcd1ce2673a63e
asdf50003206 <asdf500032061@gmail.com>
2022-09-19 14:10:01 +0800

    update config

--- config_example.py
+++ config_example.py
@@ -1,6 +1,6 @@
 DJANGO_SETTINGS='lss.settings'
 POETRY_ENVIRONMENT='/home/lss_env/lib/python3.9/site-packages'
 
-MONGO_DB_USERNAME='user_name'
-MONGO_DB_PASSWORD= 'password'
-MONGO_DB_AUTH_SOURCE= 'database'
+MONGO_DB_USERNAME='lss'
+MONGO_DB_PASSWORD= 'algo83111T%%'
+MONGO_DB_AUTH_SOURCE= 'lss'
