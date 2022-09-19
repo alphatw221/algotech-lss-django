317a9fa8280610272eff1f8dba9803015b6049e6
TIEN <tienhuifeng@accoladeglobal.net>
2022-09-19 15:11:37 +0800

    mail subject

---
+++ lib/i18n/email/notify_wishlist_email.py
@@ -0,0 +1,5 @@
+from django.utils.translation import gettext as _
+
+@lang_translate_default_en
+def i18n_get_notify_wishlist_subject(lang=None):
+    return _('EMAIL/WISHLIST/SUBJECT')
---
+++ lib/i18n/email/verification_code_email.py
@@ -0,0 +1,5 @@
+from django.utils.translation import gettext as _
+
+@lang_translate_default_en
+def i18n_get_verify_code_subject(lang=None):
+    return  _('EMAIL/VERIFY/SUBJECT')
