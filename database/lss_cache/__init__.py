f99e02cdd24bd1e56ec9f2a199507ee1ac1bd9ad
asdf50003206 <asdf500032061@gmail.com>
2022-09-19 17:34:56 +0800

    update lss cache

--- database/lss_cache/__init__.py
+++ database/lss_cache/__init__.py
@@ -1,6 +1,8 @@
 from redis import Redis
 from django.conf import settings
+import pottery
 
+AUTO_RELEASE_TIME = 2
 
 redis=Redis(host=settings.CACHE_SERVER['host'],port=settings.CACHE_SERVER['port'],password=settings.CACHE_SERVER['password'])
 
@@ -10,5 +12,37 @@ def get_key(base_key, *args):
         key+=f'_{arg}'
     return key
 
+
+def _set_dict(key, data):
+    if redis.exists(key):
+        redis.delete(key)
+    return pottery.RedisDict(data, redis=redis, key=key)
+
+def _set_list(key, data):
+    if redis.exists(key):
+        redis.delete(key)
+    return pottery.RedisList(data, redis=redis, key=key)
+
+def _leash_get_dict(key):
+    try:
+        if not redis.exists(key):
+            redis_lock = pottery.Redlock(key=key, masters={redis}, auto_release_time=AUTO_RELEASE_TIME)
+            return False, None, redis_lock
+        data = dict(pottery.RedisDict(redis=redis,key=key))
+        return True, data, None
+    except Exception:
+        return False, None, None
+
+
+
+def _leash_get_list(key):
+    try:
+        if not redis.exists(key):
+            redis_lock = pottery.Redlock(key=key, masters={redis}, auto_release_time=AUTO_RELEASE_TIME)
+            return False, None, redis_lock
+        data = list(pottery.RedisList(redis=redis,key=key))
+        return True, data, None
+    except Exception:
+        return False, None, None
 
 from . import campaign_product
--- database/lss_cache/campaign_product.py
+++ database/lss_cache/campaign_product.py
@@ -1,11 +1,13 @@
 import pottery
 
 from api.models import campaign 
-from . import redis, get_key
+from . import redis, get_key, _leash_get_dict, _leash_get_list, _set_dict, _set_list
 KEY = 'campaign_products'
 
 DATA_EXTERNAL_INTERNAL_MAP = 'external_internal_map'
 DATA_INTERNAL_EXTERNAL_MAP = 'internal_external_map'
+DATA_FOR_SELL = 'for_sale'
+DATA_ALL = 'all'
 
 AUTO_RELEASE_TIME = 2
 
@@ -48,27 +50,44 @@ def set_internal_external_map(campaign_id, plugin, internal_external_map):
 
 
 
+def leash_get_product_for_sell(campaign_id):
+    key = get_key(KEY, campaign_id, DATA_FOR_SELL)
+    return _leash_get_list(key)
+
+def set_product_for_sell(campaign_id, campaign_products):
+    key = get_key(KEY, campaign_id, DATA_FOR_SELL)
+    _set_list(key, campaign_products)
 
 
-def get(campaign_id):
-    if not redis.exists(get_key(KEY,campaign_id)):
-        return None
-    return pottery.RedisList(redis=redis,key=get_key(KEY,campaign_id))
+def leash_get_product_all(campaign_id):
+    key = get_key(KEY, campaign_id, DATA_ALL)
+    return _leash_get_list(key)
 
-def leash_get(campaign_id):
-    if redis.exists(get_key(KEY,campaign_id)):
-        return True, pottery.RedisList(redis=redis,key=get_key(KEY,campaign_id)), None
-
-    campaign_product_lock = pottery.Redlock(key=get_key(KEY,campaign_id), masters={redis}, auto_release_time=5)
-    return False, None, campaign_product_lock
-
-def set(campaign_id, campaign_products):
-    if redis.exists(get_key(KEY,campaign_id)):
-        redis.delete(get_key(KEY,campaign_id))
-    return pottery.RedisList(campaign_products, redis=redis, key=get_key(KEY,campaign_id))
+def set_product_all(campaign_id, campaign_products):
+    key = get_key(KEY, campaign_id, DATA_ALL)
+    _set_list(key, campaign_products)
 
 
-def invalidate(*args):
-    key = get_key(KEY,*args)
-    print(key)
-    redis.delete(key)
+
+# def get(campaign_id):
+#     if not redis.exists(get_key(KEY,campaign_id)):
+#         return None
+#     return pottery.RedisList(redis=redis,key=get_key(KEY,campaign_id))
+
+# def leash_get(campaign_id):
+#     if redis.exists(get_key(KEY,campaign_id)):
+#         return True, pottery.RedisList(redis=redis,key=get_key(KEY,campaign_id)), None
+
+#     campaign_product_lock = pottery.Redlock(key=get_key(KEY,campaign_id), masters={redis}, auto_release_time=5)
+#     return False, None, campaign_product_lock
+
+# def set(campaign_id, campaign_products):
+#     if redis.exists(get_key(KEY,campaign_id)):
+#         redis.delete(get_key(KEY,campaign_id))
+#     return pottery.RedisList(campaign_products, redis=redis, key=get_key(KEY,campaign_id))
+
+
+# def invalidate(*args):
+#     key = get_key(KEY,*args)
+#     print(key)
+#     redis.delete(key)
