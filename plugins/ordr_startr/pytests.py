dc95e1bb7d97b2a9cc7061ac592770afb609c778
asdf50003206 <asdf500032061@gmail.com>
2022-09-25 18:06:20 +0800

    update setting

--- plugins/ordr_startr/pytests.py
+++ plugins/ordr_startr/pytests.py
@@ -28,19 +28,48 @@ import pytest
 #     assert 1 == 1
 
 from automation.jobs.ordr_startr import export_product_job
-class TestProductSync(object):
+from api import models
+import database
 
-    def setup_class(self):
-        print("setup_class(self)：每個類之前執行一次,只執行一次")
 
-    def teardown_class(self):
-        print("teardown_class(self)：每個類之後執行一次,只執行一次")
+@pytest.mark.django_db(reset_sequences=True)
+def test_a():
+    # print("setup_class(self)：每個類之前執行一次,只執行一次")
+    print('test_a')
+    # print(models.campaign.campaign.Campaign.objects.filter(id=1000).first())
+    # campaigns = models.campaign.campaign.Campaign.objects.all()
+    # for campaign in campaigns:
+    #     print(campaign)
+    campaign = models.campaign.campaign.Campaign.objects.create()
+    print(campaign.id)
 
-    def test_valid(self):
-        print("正在執行用例A")
-        x = "this"
-        assert 'h' in x
+    # print(database.lss.campaign.Campaign.filter(id=1000))
 
-    def test_invalid(self):
-        print("正在執行B")
-        assert 1 == 1
+
+# pytestmark = pytest.mark.django_db
+
+
+# @pytest.mark.django_db
+# class TestProductSync(object):
+
+#     pytestmark = pytest.mark.django_db
+
+#     @pytest.mark.django_db
+#     def setup_class(self):
+#         print("setup_class(self)：每個類之前執行一次,只執行一次")
+#         print(models.campaign.campaign.Campaign.objects.filter(id=1000).first())
+#         # print(database.lss.campaign.Campaign.filter(id=1000))
+
+#     def teardown_class(self):
+#         print("teardown_class(self)：每個類之後執行一次,只執行一次")
+
+#     @pytest.mark.django_db
+#     def test_valid(self):
+#         print("正在執行用例A")
+#         x = "this"
+#         assert 'h' in x
+
+#     @pytest.mark.django_db
+#     def test_invalid(self):
+#         print("正在執行B")
+#         assert 1 == 1
