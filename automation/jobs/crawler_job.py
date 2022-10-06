4a0d958d2605c6855ef98555eb88ef568045703e
Nick LIen <nicklien@accoladeglobal.net>
2022-10-06 17:32:30 +0800

    update crawler job error handler

--- automation/jobs/crawler_job.py
+++ automation/jobs/crawler_job.py
@@ -1,6 +1,7 @@
 import os
 import config
 import django
+import database
 
 from lib.helper.lucky_draw_helper import FacebookSharedListCrawler
 try:
@@ -15,17 +16,13 @@ from datetime import datetime
 import traceback
 
 
-@lib.error_handle.error_handler.twitch_chat_job_error_handler.twitch_chat_job_error_handler
+@lib.error_handle.error_handler.crawler_job_error_handler.crawler_job_error_handler
 def crawler_shared_post_job(lucky_draw_id, facebook_page_username, post_id):
-    try:
-        print("start crawler")
-    except Exception as e:
-        print(traceback.format_exc())
-        
-    # lucky_draw = database.lss.lucky_draw.CampaignLuckyDraw.get_object(id = lucky_draw_id)
-    # fb_crawler = FacebookSharedListCrawler(facebook_page_username, post_id)
-    # shared_user_name_set = fb_crawler.start()
-    # meta = lucky_draw['meta']
-    # meta["shared_post_data"] = list(shared_user_name_set)
-    # print(meta)
-    # database.lss.lucky_draw.CampaignLuckyDraw.update(meta=meta)
+    print("start crawler")
+    lucky_draw = database.lss.lucky_draw.CampaignLuckyDraw.get_object(id = lucky_draw_id)
+    fb_crawler = FacebookSharedListCrawler(facebook_page_username, post_id)
+    shared_user_name_set = fb_crawler.start()
+    meta = lucky_draw['meta']
+    meta["shared_post_data"] = list(shared_user_name_set)
+    print(meta)
+    database.lss.lucky_draw.CampaignLuckyDraw.update(meta=meta)
