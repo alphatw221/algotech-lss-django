646d56593a5159b2cecdd7b71695f7ab0983aa55
林奕學 <lin@linyixuedeMac-mini.local>
2022-10-11 11:32:57 +0800

    update script

--- shell_script/deploy-backend.sh
+++ shell_script/deploy-backend.sh
@@ -3,10 +3,11 @@
 cd /home/liveshowseller
 git checkout $1
 git pull http://lin:a334596412@algotech-git.ap.ngrok.io/nicklien/liveshowseller.git $1
-
+echo 'a'
 poetry install
-
+echo 'b'
 service supervisor stop
+echo 'c'
 service supervisor start
 supervisorctl reread
 supervisorctl reload
