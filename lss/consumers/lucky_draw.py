4081bd1cdb8031a3457acb9f08d900c1a6bdf113
Nick <nicklien@accoladeglobal.net>
2022-10-27 18:00:47 +0800

    update

--- lss/consumers/lucky_draw.py
+++ lss/consumers/lucky_draw.py
@@ -14,9 +14,6 @@ class SharedPostCrawlerConsumer(WebsocketConsumer):
         campaign_lucky_draw_id = self.scope['url_route']['kwargs']['campaign_lucky_draw_id']
         api_user = lib.util.verify.Verify.get_seller_user_from_scope(self.scope)
         user_subscription = lib.util.verify.Verify.get_user_subscription_from_api_user(api_user)
-        lucky_draw = lib.util.verify.Verify.get_lucky_draw(campaign_lucky_draw_id)
-        username = lucky_draw.campaign.facebook_page.username
-        post_id = lucky_draw.campaign.facebook_campaign.get("post_id", "")
         self.room_group_name = 'campaign_lucky_draw_%s' % campaign_lucky_draw_id
         
         async_to_sync(self.channel_layer.group_add)(
@@ -24,15 +21,22 @@ class SharedPostCrawlerConsumer(WebsocketConsumer):
             self.channel_name
         )
         self.accept()
-        service.rq.queue.enqueue_crawler_queue(job=crawler_shared_post_job, room_id=self.room_group_name, lucky_draw_id=lucky_draw.id, facebook_page_username=username, post_id=post_id)
+        
     def disconnect(self, close_code):
 
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
-
-
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        action = text_data_json.get("action")
+        if action == "start_crawler":
+            campaign_lucky_draw_id = text_data_json.get('lucky_draw_id')
+            lucky_draw = lib.util.verify.Verify.get_lucky_draw(campaign_lucky_draw_id)
+            username = lucky_draw.campaign.facebook_page.username
+            post_id = lucky_draw.campaign.facebook_campaign.get("post_id", "")
+            service.rq.queue.enqueue_crawler_queue(job=crawler_shared_post_job, room_id=self.room_group_name, lucky_draw_id=lucky_draw.id, facebook_page_username=username, post_id=post_id)
     def success_data(self, event):
         self.send(text_data=json.dumps(event))
 
