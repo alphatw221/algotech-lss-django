47a222e08dad27a2ceec5dbafec6a13b3fa263d0
test <test@email.com>
2022-10-28 13:45:25 +0800

    move campaign reminder script

--- shell_script/campaign_remind.sh
+++ shell_script/campaign_remind.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd /home/agt-e5-2670-v3/liveshowseller
+cd "$(dirname "$0")" && cd ..
+pwd
 # source /root/.cache/pypoetry/virtualenvs/liveshowseller-PfxpePKz-py3.8/bin/activate
-poetry run python manage_test.py runcrons
+poetry run python manage.py runcrons
