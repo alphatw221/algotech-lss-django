051c55103f1dd7d5cee481208fa5b86bed3a9533
林奕學 <lin@linyixuedeMac-mini.local>
2022-11-25 15:06:30 +0800

    update

--- database/lss/order.py
+++ database/lss/order.py
@@ -94,13 +94,13 @@ def get_wallet_data_with_expired_points(start_from = None, end_at = None):
     point_expired_at_filter_query = {"$ne":None}
 
     if start_from:
-        point_expired_at_filter_query["$gt"] = start_from
+        point_expired_at_filter_query["$gte"] = start_from
 
     if end_at:
-        point_expired_at_filter_query["$lt"] = end_at
+        point_expired_at_filter_query["$lte"] = end_at
     
     query = [
-            {"$match":{"point_expired_at":point_expired_at_filter_query, "buyer_id":{"$ne":None}}},
+            {"$match":{"point_expired_at":point_expired_at_filter_query, "buyer_id":{"$ne":None}}, "point_expired_calculated":False},
             {
                 "$lookup": 
                 {
@@ -141,7 +141,7 @@ def get_wallet_data_with_expired_points(start_from = None, end_at = None):
     return l 
 
 
-def get_total_earned_used_expired_points(buyer_id, user_subscription_id, order_created_after=None, ):
+def get_earned_used_expired_points_sum(buyer_id, user_subscription_id, order_created_after=None, ):
 
     
     query = [
@@ -169,7 +169,8 @@ def get_total_earned_used_expired_points(buyer_id, user_subscription_id, order_c
                                 {
                                     "$or":[
                                         {"$gt": ["$point_expired_at",datetime.utcnow()]},
-                                        {"$eq": ["$point_expired_at",None]}
+                                        {"$eq": ["$point_expired_at",None]},
+                                        {"$eq": ["$point_expired_calculated",True]}
                                     ]
                                 },
                                 0,
@@ -185,8 +186,20 @@ def get_total_earned_used_expired_points(buyer_id, user_subscription_id, order_c
     l = list(cursor)
     return l[0].get('total_points_earned',0) if l else 0, l[0].get('total_points_used',0) if l else 0, l[0].get('total_points_expired',0) if l else 0,
 
-def mark_order_points_used_calculated(start_from=datetime.utcnow()):
-    __collection.update_many({"created_at":{"$gt":start_from}},{"$set":{"points_used_calculated":True}})
+def mark_order_points_used_calculated(buyer_id, start_from=datetime.utcnow()):
+    assert buyer_id != None
+    __collection.update_many({"buyer_id":buyer_id, "created_at":{"$gt":start_from}},{"$set":{"points_used_calculated":True}})
+
+def mark_order_point_expired_calculated(buyer_id, start_from=datetime.utcnow()):
+    assert buyer_id != None
+    __collection.update_many({
+            "buyer_id":buyer_id,
+            "created_at":{"$gt":start_from},
+            "point_expired_at":{"$ne":None, "$lte":datetime.utcnow()}
+        },
+        {
+            "$set":{"points_used_calculated":True}
+        })
 
 
 def get_wallet_data(start_from = None, end_at = None ):
