b8912e6fdf1f1e076698fb21fea01db19462f4f5
林奕學 <lin@linyixuedeMac-mini.local>
2022-11-25 17:17:53 +0800

    update

--- lss/settings_dev.py
+++ lss/settings_dev.py
@@ -16,3 +16,41 @@ GOOGLE_OAUTH_CLIENT_SECRET_FOR_LIVESHOWSELLER = "GOCSPX-eBlnf5ZN4tPveGcNxg4Slkup
 STRIPE_API_KEY = "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"
 #Recaptcha V2
 RECAPTCHA_SECRET_KEY='6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'lss_dev',       
+#         'ENFORCE_SCHEMA': False,
+#         'CLIENT': {
+#             'host': 'mongodb://34.126.92.142:27017, 35.240.200.4:27017, 34.126.155.150:27017',
+#             'replicaSet': 'rs0',
+#             'username': config.MONGO_DB_USERNAME,
+#             'password': config.MONGO_DB_PASSWORD,
+#             'authSource': 'lss_dev',
+#             'authMechanism': 'SCRAM-SHA-1',
+#             'readPreference': 'secondaryPreferred',
+#             'ssl': False,
+#         }
+#     },
+# }
+
+# MONGODB_CONNECTION_STRING = \
+#     'mongodb://'+config.MONGO_DB_USERNAME+':'+config.MONGO_DB_PASSWORD+\
+#     '@34.126.92.142:27017,35.240.200.4:27017,34.126.155.150:27017/'+\
+#     '?authSource=lss_dev'
+    
+# MONGODB_DATABASE_NAME = 'lss_dev'
+
+
+# REDIS_SERVER = {
+#     "host": "127.0.0.1",
+#     "port": "6379",
+#     "username": None,
+#     "password": r"algo83111T%%"}
+
+# CACHE_SERVER = {
+#     "host": "127.0.0.1",
+#     "port": "6380",
+#     "username": None,
+#     "password": r"algo83111T%%"}
