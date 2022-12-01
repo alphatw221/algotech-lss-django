ad92586d0a11414d0be18f8776aed57823881272
林奕學 <lin@linyixuedeMac-mini.local>
2022-12-01 17:15:09 +0800

    update

--- api_v2/views/facebook/facebook.py
+++ api_v2/views/facebook/facebook.py
@@ -13,10 +13,14 @@ import json
 def facebook_messenger_webhook(request):
     try:
         if request.method == 'GET':
+            
             mode = request.query_params.get('hub.mode', None)
             token = request.query_params.get('hub.verify_token', None)
             challenge = request.query_params.get('hub.challenge', None)
 
+            print(mode)
+            print(token)
+            
             if mode == 'subscribe' and token == settings.CHAT_BOT_FACEBOOK['VERIFY_TOKEN']:
                 return HttpResponse(challenge, status=200)
 
