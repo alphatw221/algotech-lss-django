5cf12fa98a3a0b32ba108a87633566dd21952b53
Nick Lien <nicklien@accoladeglobal.net>
2022-12-21 14:12:30 +0800

    add meta_variant, pinned fields

--- api/models/campaign/campaign_product.py
+++ api/models/campaign/campaign_product.py
@@ -92,6 +92,8 @@ class CampaignProduct(models.Model):
     meta_logistic = models.JSONField(default=dict, null=True, blank=True)
     tag = models.JSONField(default=dict, null=True, blank=True)
     categories = models.JSONField(default=list, null=True, blank=True)
+    pinned = models.BooleanField(null=False, default=False)
+    meta_variant = models.JSONField(default=dict, null=False, blank=True)
 
 
 class CampaignProductSerializer(serializers.ModelSerializer):
