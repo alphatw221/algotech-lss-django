ff729e3c6b44552d6cdaae0c5812bab0065d93f3
林奕學 <lin@linyixuedeMac-mini.local>
2022-12-21 17:56:05 +0800

    update

---
+++ lss/settings_prod_local.py
@@ -0,0 +1,10 @@
+from lss.settings import *
+
+DEBUG = True
+WEB_SERVER_URL = "https://localhost:3000"
+SHOPPING_CART_URL = f"{WEB_SERVER_URL}/buyer/cart"
+
+# gcp load balancer
+GCP_API_LOADBALANCER_URL = "http://localhost:8001"
+
+
