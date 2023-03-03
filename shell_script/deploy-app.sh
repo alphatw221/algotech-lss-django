291e9995143405cd3a866f4565237d6de9a1b55b
lin <yihsuehlin@accoladeglobal.com>
2023-03-03 13:39:33 +0800

    update

--- shell_script/deploy-app.sh
+++ shell_script/deploy-app.sh
@@ -12,7 +12,7 @@ git checkout $1
 # git pull http://lin:a334596412@algotech-git.ap.ngrok.io/nicklien/liveshowseller.git $1
 git pull https://alphatw221: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@github.com/alphatw221/algotech-lss-django.git $1
 
-poetry install
+# poetry install
 sudo mkdir /run/daphne/
 poetry run python manage.py collectstatic --noinput
 service supervisor stop
